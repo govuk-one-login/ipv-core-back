7b04df835cf94b75055f60784243910e91ef966e
Kerr Rainey <kerr.rainey@digital.cabinet-office.gov.uk>
2021-11-04 15:01:25 +0000

    PYI-297: Add lambda resources
    Add terraform modules for an `authorize` lambda and a lambda iam role.

--- terraform/lambda/api-gateway.tf
+++ terraform/lambda/api-gateway.tf
@@ -3,3 +3,23 @@ resource "aws_api_gateway_rest_api" "ipv_internal" {
 
   tags = local.default_tags
 }
+
+
+resource "aws_lambda_function" "authorize" {
+  filename         = var.lambda_zip_file
+  function_name    = "${var.environment}-authorize"
+  role             = aws_iam_role.lambda_iam_role.arn
+  handler          = "uk.gov.di.ipv.lambda.AuthorizationHandler::handleRequest"
+  runtime          = "java11"
+  source_code_hash = filebase64sha256(var.lambda_zip_file)
+  publish          = true
+  timeout          = 30
+  memory_size      = 2048
+}
+
+resource "aws_lambda_alias" "authorize_active" {
+  name             = "${var.environment}-active"
+  description      = "Alias pointing at active version of Lambda"
+  function_name    = aws_lambda_function.authorize.arn
+  function_version = aws_lambda_function.authorize.version
+}
