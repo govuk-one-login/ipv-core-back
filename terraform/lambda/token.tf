64801c5541add531cabfe47e02dda857f84ec603
dannyhayes-gds <daniel.hayes@digital.cabinet-office.gov.uk>
2021-11-16 13:04:43 +0000

    Update token rest endpoint to use POST

--- terraform/lambda/token.tf
+++ terraform/lambda/token.tf
@@ -5,7 +5,7 @@ module "token" {
   rest_api_id            = aws_api_gateway_rest_api.ipv_internal.id
   rest_api_execution_arn = aws_api_gateway_rest_api.ipv_internal.execution_arn
   root_resource_id       = aws_api_gateway_rest_api.ipv_internal.root_resource_id
-  http_method            = "GET"
+  http_method            = "POST"
   path_part              = "token"
   handler                = "uk.gov.di.ipv.lambda.AccessTokenHandler::handleRequest"
   function_name          = "${var.environment}-token"
