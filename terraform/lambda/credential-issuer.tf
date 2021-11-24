7a9b3105d172d209ff23f8805347f25a34baef62
Phil Miller <phillip.miller@digital.cabinet-office.gov.uk>
2021-11-24 12:24:51 +0000

    Update credential issuer url part to /request-evidence, to match our sequence diagram

--- terraform/lambda/credential-issuer.tf
+++ terraform/lambda/credential-issuer.tf
@@ -6,7 +6,7 @@ module "credential-issuer" {
   rest_api_execution_arn = aws_api_gateway_rest_api.ipv_internal.execution_arn
   root_resource_id       = aws_api_gateway_rest_api.ipv_internal.root_resource_id
   http_method            = "POST"
-  path_part              = "credential-issuer"
+  path_part              = "request-evidence"
   handler                = "uk.gov.di.ipv.lambda.CredentialIssuerHandler::handleRequest"
   function_name          = "${var.environment}-credential-issuer"
   role_name              = "${var.environment}-credential-issuer-role"
