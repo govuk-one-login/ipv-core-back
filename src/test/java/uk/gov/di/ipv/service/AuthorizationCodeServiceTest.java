d03be8d9e2b41fb33f50849819c1866d7350f505
Daniel Hayes <daniel.hayes@digital.cabinet-office.gov.uk>
2021-12-06 15:26:39 +0000

    PYI-397: Add missing test cases

--- src/test/java/uk/gov/di/ipv/service/AuthorizationCodeServiceTest.java
+++ src/test/java/uk/gov/di/ipv/service/AuthorizationCodeServiceTest.java
@@ -9,6 +9,7 @@ import uk.gov.di.ipv.persistence.item.AuthorizationCodeItem;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -47,5 +48,42 @@ class AuthorizationCodeServiceTest {
         verify(mockDataStore).create(authorizationCodeItemArgumentCaptor.capture());
         assertEquals(ipvSessionId, authorizationCodeItemArgumentCaptor.getValue().getIpvSessionId());
         assertEquals(testCode.getValue(), authorizationCodeItemArgumentCaptor.getValue().getAuthCode());
+    }
+
+    @Test
+    void shouldGetSessionIdByAuthCodeWhenValidAuthCodeProvided() {
+        AuthorizationCode testCode = new AuthorizationCode();
+        String ipvSessionId = "session-12345";
+
+        AuthorizationCodeItem testItem = new AuthorizationCodeItem();
+        testItem.setIpvSessionId(ipvSessionId);
+
+        when(mockDataStore.getItem(testCode.getValue())).thenReturn(testItem);
+
+        String resultIpvSessionid = authorizationCodeService.getIpvSessionIdByAuthorizationCode(testCode.getValue());
+
+        verify(mockDataStore).getItem(testCode.getValue());
+        assertEquals(ipvSessionId, resultIpvSessionid);
+    }
+
+    @Test
+    void shouldReturnNullWhenInvalidAuthCodeProvided() {
+        AuthorizationCode testCode = new AuthorizationCode();
+
+        when(mockDataStore.getItem(testCode.getValue())).thenReturn(null);
+
+        String resultIpvSessionid = authorizationCodeService.getIpvSessionIdByAuthorizationCode(testCode.getValue());
+
+        verify(mockDataStore).getItem(testCode.getValue());
+        assertNull(resultIpvSessionid);
+    }
+
+    @Test
+    void shouldCallDeleteWithAuthCode() {
+        AuthorizationCode testCode = new AuthorizationCode();
+
+        authorizationCodeService.revokeAuthorizationCode(testCode.getValue());
+
+        verify(mockDataStore).delete(testCode.getValue());
     }
 }
