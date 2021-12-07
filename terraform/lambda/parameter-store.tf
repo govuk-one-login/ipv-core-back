299397d3f8b763c60a3d2058bba9e5e58a0780a1
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2021-12-07 17:00:16 +0000

    PYI-423: Give credential-issuer lambda ssm perms
    The crednetial-issuer lambda now pulls config from the parameter store.
    Without giving it the correct permissions it won't be able to do this.

--- terraform/lambda/parameter-store.tf
+++ terraform/lambda/parameter-store.tf
@@ -4,3 +4,22 @@ resource "aws_ssm_parameter" "credential-issuers-config" {
   value     = var.credential_issuers_config
   overwrite = true
 }
+
+resource "aws_iam_role_policy" "get-credential-issuers-config" {
+  name = "get-credential-issuers-config"
+  role = module.credential-issuer.iam_role_arn
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid = "GetCredentialIssuersConfig"
+        Action = [
+          "ssm:GetParameter"
+        ]
+        Effect   = "Allow"
+        Resource = aws_ssm_parameter.credential-issuers-config.arn
+      }
+    ]
+  })
+}
