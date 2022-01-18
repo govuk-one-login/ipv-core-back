10e17ac9b33b32900d958b61cb3842a197048abd
tomtucka <tomtucka@gmail.com>
2022-01-18 10:15:43 +0000

    PYI-537 - Update for_each to get each object

--- terraform/modules/credential_issuer/main.tf
+++ terraform/modules/credential_issuer/main.tf
@@ -1,14 +1,14 @@
 resource "aws_ssm_parameter" "token_url" {
-  for_each  = toset(var.issuers)
-  name      = "/${var.environment}/IPV/Core/CredentialIssuers/${each.value.id}/tokenUrl"
+  for_each  = {for c in var.issuers : c.id => c}
+  name      = "/${var.environment}/ipv/core/credentialIssuers/${each.value.id}/tokenUrl"
   type      = var.type
   value     = each.value.token_url
   overwrite = var.overwrite
 }
 
 resource "aws_ssm_parameter" "credential_url" {
-  for_each  = toset(var.issuers)
-  name      = "/${var.environment}/IPV/Core/CredentialIssuers/${each.value.id}/credentialUrl"
+  for_each  = {for c in var.issuers : c.id => c}
+  name      = "/${var.environment}/ipv/core/credentialIssuers/${each.value.id}/credentialUrl"
   type      = var.type
   value     = each.value.credential_url
   overwrite = var.overwrite
