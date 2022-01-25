2d933d1bc81753b2f289d6463db2288dd85ded4f
Danny Hayes <daniel.hayes@digital.cabinet-office.gov.uk>
2022-01-25 13:06:07 +0000

    Fix sonar lint code coverage issue on getter and setter methods

--- lib/src/main/java/uk/gov/di/ipv/core/library/domain/DebugCredentialAttributes.java
+++ lib/src/main/java/uk/gov/di/ipv/core/library/domain/DebugCredentialAttributes.java
@@ -1,27 +1,16 @@
 package uk.gov.di.ipv.core.library.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class DebugCredentialAttributes {
     private String ipvSessionId;
     private String dateCreated;
 
     public DebugCredentialAttributes(String ipvSessionId, String dateCreated) {
         this.ipvSessionId = ipvSessionId;
-        this.dateCreated = dateCreated;
-    }
-
-    public String getIpvSessionId() {
-        return ipvSessionId;
-    }
-
-    public void setIpvSessionId(String ipvSessionId) {
-        this.ipvSessionId = ipvSessionId;
-    }
-
-    public String getDateCreated() {
-        return dateCreated;
-    }
-
-    public void setDateCreated(String dateCreated) {
         this.dateCreated = dateCreated;
     }
 }
--- lib/src/main/java/uk/gov/di/ipv/core/library/domain/UserIssuedDebugCredential.java
+++ lib/src/main/java/uk/gov/di/ipv/core/library/domain/UserIssuedDebugCredential.java
@@ -1,9 +1,13 @@
 package uk.gov.di.ipv.core.library.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Map;
 
+@Getter
+@Setter
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class UserIssuedDebugCredential {
 
@@ -13,21 +17,5 @@ public class UserIssuedDebugCredential {
     public UserIssuedDebugCredential(DebugCredentialAttributes attributes) {
         this.attributes = attributes;
         this.gpg45Score = null;
-    }
-
-    public DebugCredentialAttributes getAttributes() {
-        return attributes;
-    }
-
-    public void setAttributes(DebugCredentialAttributes attributes) {
-        this.attributes = attributes;
-    }
-
-    public Map<String, String> getGpg45Score() {
-        return gpg45Score;
-    }
-
-    public void setGpg45Score(Map<String, String> gpg45Score) {
-        this.gpg45Score = gpg45Score;
     }
 }
