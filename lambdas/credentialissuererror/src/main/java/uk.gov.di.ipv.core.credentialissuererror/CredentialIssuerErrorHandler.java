6cd822c01b75c03244f350d9fb8b9fe71f9746d5
Danny Hayes <daniel.hayes@digital.cabinet-office.gov.uk>
2022-03-17 10:10:47 +0000

    PYIC-402: Create new lambda within SAM for the cri error endpoint

---
+++ lambdas/credentialissuererror/src/main/java/uk.gov.di.ipv.core.credentialissuererror/CredentialIssuerErrorHandler.java
@@ -0,0 +1,14 @@
+package uk.gov.di.ipv.core.credentialissuererror;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+public class CredentialIssuerErrorHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+        return null;
+    }
+}
