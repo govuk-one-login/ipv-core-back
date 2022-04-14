028da46bf1f273cb4f30ba378285efd80e28a683
Danny Hayes <daniel.hayes@digital.cabinet-office.gov.uk>
2022-04-14 17:33:16 +0100

    Update timestamp field in message payload to be an int

--- lib/src/main/java/uk/gov/di/ipv/core/library/auditing/AuditEvent.java
+++ lib/src/main/java/uk/gov/di/ipv/core/library/auditing/AuditEvent.java
@@ -8,14 +8,14 @@ import uk.gov.di.ipv.core.library.annotations.ExcludeFromGeneratedCoverageReport
 @ExcludeFromGeneratedCoverageReport
 @Getter
 public class AuditEvent {
-    @JsonProperty private long timestamp;
+    @JsonProperty private int timestamp;
 
     @JsonProperty("event_name")
     private AuditEventTypes event;
 
     @JsonCreator
     public AuditEvent(
-            @JsonProperty(value = "timestamp", required = true) long timestamp,
+            @JsonProperty(value = "timestamp", required = true) int timestamp,
             @JsonProperty(value = "event_name", required = true) AuditEventTypes event) {
         this.timestamp = timestamp;
         this.event = event;
