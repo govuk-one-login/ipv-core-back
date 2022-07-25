b30bbaed51fe88dc7422f72c7f7ca8a62d47798d
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2022-07-25 14:26:05 +0100

    PYIC-1688: Create evaluate-gpg45-scores λ skeleton
    This should be the minimal change to allow the new lambda to be built
    and deployed. It won't actually do anything yet. Provides a base to
    build on.

---
+++ lambdas/evaluate-gpg45-scores/src/main/java/uk/gov/di/ipv/core/evaluategpg45scores/EvaluateGpg45ScoresHandler.java
@@ -0,0 +1,15 @@
+package uk.gov.di.ipv.core.evaluategpg45scores;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+public class EvaluateGpg45ScoresHandler
+        implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(
+            APIGatewayProxyRequestEvent input, Context context) {
+        return null;
+    }
+}
