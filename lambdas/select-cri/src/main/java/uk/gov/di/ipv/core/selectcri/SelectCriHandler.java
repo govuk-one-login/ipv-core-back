86a683a6cc7a5c3c7a2c37b8fb1fa9fccd6915b3
Daniel Pomfret <daniel.pomfret@digital.cabinet-office.gov.uk>
2022-08-01 09:38:06 +0100

    PYIC-1720: Create new Select CRI Lambda Java Code.

---
+++ lambdas/select-cri/src/main/java/uk/gov/di/ipv/core/selectcri/SelectCriHandler.java
@@ -0,0 +1,61 @@
+package uk.gov.di.ipv.core.selectcri;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.nimbusds.jose.JWSSigner;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import software.amazon.lambda.powertools.logging.Logging;
+import software.amazon.lambda.powertools.tracing.Tracing;
+import uk.gov.di.ipv.core.library.annotations.ExcludeFromGeneratedCoverageReport;
+import uk.gov.di.ipv.core.library.helpers.KmsEs256Signer;
+import uk.gov.di.ipv.core.library.helpers.LogHelper;
+import uk.gov.di.ipv.core.library.service.AuditService;
+import uk.gov.di.ipv.core.library.service.ConfigurationService;
+import uk.gov.di.ipv.core.library.service.IpvSessionService;
+import uk.gov.di.ipv.core.library.service.UserIdentityService;
+
+public class SelectCriHandler
+        implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    private final ConfigurationService configurationService;
+    private final UserIdentityService userIdentityService;
+    private final JWSSigner signer;
+    private final AuditService auditService;
+    private final IpvSessionService ipvSessionService;
+
+    public SelectCriHandler(
+            ConfigurationService configurationService,
+            UserIdentityService userIdentityService,
+            JWSSigner signer,
+            AuditService auditService,
+            IpvSessionService ipvSessionService) {
+        this.configurationService = configurationService;
+        this.userIdentityService = userIdentityService;
+        this.signer = signer;
+        this.auditService = auditService;
+        this.ipvSessionService = ipvSessionService;
+    }
+
+    @ExcludeFromGeneratedCoverageReport
+    public SelectCriHandler() {
+        this.configurationService = new ConfigurationService();
+        this.userIdentityService = new UserIdentityService(configurationService);
+        this.signer = new KmsEs256Signer(configurationService.getSigningKeyId());
+        this.auditService =
+                new AuditService(AuditService.getDefaultSqsClient(), configurationService);
+        this.ipvSessionService = new IpvSessionService(configurationService);
+    }
+
+    @Override
+    @Tracing
+    @Logging(clearState = true)
+    public APIGatewayProxyResponseEvent handleRequest(
+            APIGatewayProxyRequestEvent input, Context context) {
+        LogHelper.attachComponentIdToLogs();
+        return null;
+    }
+}
