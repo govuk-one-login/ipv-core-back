9f8f40c209d1a436911261098d89b35141ca5feb
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2022-08-22 16:26:58 +0100

    PYIC-1834: Use `@SerializedName`
    We can use this annotation to ensure that the Java object field names
    are consistent with the usual patterns.

--- lambdas/contra-indicator-storage-core-stub/src/main/java/uk/gov/di/ipv/core/contraindicatorstoragecorestub/domain/GetCiRequest.java
+++ lambdas/contra-indicator-storage-core-stub/src/main/java/uk/gov/di/ipv/core/contraindicatorstoragecorestub/domain/GetCiRequest.java
@@ -1,17 +1,20 @@
 package uk.gov.di.ipv.core.contraindicatorstoragecorestub.domain;
 
+import com.google.gson.annotations.SerializedName;
 import lombok.Getter;
 import uk.gov.di.ipv.core.library.annotations.ExcludeFromGeneratedCoverageReport;
 
 @ExcludeFromGeneratedCoverageReport
 @Getter
-@SuppressWarnings("java:S116") // Field names should comply with a naming convention
 public class GetCiRequest {
-    private final String govuk_signin_journey_id;
-    private final String user_id;
+    @SerializedName(value = "govuk_signin_journey_id")
+    private final String govukSigninJourneyId;
+
+    @SerializedName(value = "user_id")
+    private final String userId;
 
     public GetCiRequest(String govUkSigningJourneyId, String userId) {
-        this.govuk_signin_journey_id = govUkSigningJourneyId;
-        this.user_id = userId;
+        this.govukSigninJourneyId = govUkSigningJourneyId;
+        this.userId = userId;
     }
 }
--- lambdas/contra-indicator-storage-core-stub/src/main/java/uk/gov/di/ipv/core/contraindicatorstoragecorestub/domain/PutCiRequest.java
+++ lambdas/contra-indicator-storage-core-stub/src/main/java/uk/gov/di/ipv/core/contraindicatorstoragecorestub/domain/PutCiRequest.java
@@ -1,17 +1,20 @@
 package uk.gov.di.ipv.core.contraindicatorstoragecorestub.domain;
 
+import com.google.gson.annotations.SerializedName;
 import lombok.Getter;
 import uk.gov.di.ipv.core.library.annotations.ExcludeFromGeneratedCoverageReport;
 
 @Getter
 @ExcludeFromGeneratedCoverageReport
-@SuppressWarnings("java:S116") // Field names should comply with a naming convention
 public class PutCiRequest {
-    private final String govuk_signin_journey_id;
-    private final String signed_jwt;
+    @SerializedName(value = "govuk_signin_journey_id")
+    private final String govukSigninJourneyId;
+
+    @SerializedName(value = "signed_jwt")
+    private final String signedJwt;
 
     public PutCiRequest(String govUkSigningJourneyId, String signedJwt) {
-        this.govuk_signin_journey_id = govUkSigningJourneyId;
-        this.signed_jwt = signedJwt;
+        this.govukSigninJourneyId = govUkSigningJourneyId;
+        this.signedJwt = signedJwt;
     }
 }
