4d81c640afe911f645531ba0cf1da10eb3d64b24
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2022-09-26 17:32:15 +0100

    BAU: Clean up terraform dir
    We don't use it anymore.

--- terraform/modules/endpoint/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "trigger" {
-  description = "arbitrary value which changes when the deployment needs to be retriggered"
-  value       = sha1(jsonencode(aws_api_gateway_integration.endpoint_integration))
-}
-
-output "iam_role_id" {
-  description = "The ID of the IAM role used by the lambda"
-  value       = aws_iam_role.lambda_iam_role.id
-}
