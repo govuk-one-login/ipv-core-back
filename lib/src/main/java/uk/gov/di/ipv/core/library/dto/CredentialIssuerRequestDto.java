ecd4215a5a4b15ce93a8b423caef3a3c262662bc
Patrick Blakey <patrick.blakey@digital.cabinet-office.gov.uk>
2022-10-07 13:55:12 +0100

    PYIC-2044 Add error params to CredentialIssuerRequestDto

--- lib/src/main/java/uk/gov/di/ipv/core/library/dto/CredentialIssuerRequestDto.java
+++ lib/src/main/java/uk/gov/di/ipv/core/library/dto/CredentialIssuerRequestDto.java
@@ -18,16 +18,24 @@ public class CredentialIssuerRequestDto {
 
     private final String state;
 
+    private final String error;
+
+    private final String errorDescription;
+
     public CredentialIssuerRequestDto(
             @JsonProperty(value = "authorization_code") String authorizationCode,
             @JsonProperty(value = "credential_issuer_id") String credentialIssuerId,
             @JsonProperty(value = "ipv_session_id") String ipvSessionId,
             @JsonProperty(value = "redirect_uri") String redirectUri,
-            @JsonProperty(value = "state") String state) {
+            @JsonProperty(value = "state") String state,
+            @JsonProperty(value = "error") String error,
+            @JsonProperty(value = "error_description") String errorDescription) {
         this.authorizationCode = authorizationCode;
         this.credentialIssuerId = credentialIssuerId;
         this.ipvSessionId = ipvSessionId;
         this.redirectUri = redirectUri;
         this.state = state;
+        this.error = error;
+        this.errorDescription = errorDescription;
     }
 }
