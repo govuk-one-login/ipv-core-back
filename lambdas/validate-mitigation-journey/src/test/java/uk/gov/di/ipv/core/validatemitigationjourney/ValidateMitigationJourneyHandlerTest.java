40e058219a04f17b5471301befe87caaa2836b54
Danny Hayes <daniel.hayes@digital.cabinet-office.gov.uk>
2022-10-26 12:04:32 +0100

    Fix test coverage

---
+++ lambdas/validate-mitigation-journey/src/test/java/uk/gov/di/ipv/core/validatemitigationjourney/ValidateMitigationJourneyHandlerTest.java
@@ -0,0 +1,31 @@
+package uk.gov.di.ipv.core.validatemitigationjourney;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+class ValidateMitigationJourneyHandlerTest {
+    @Mock private Context mockContext;
+
+    @InjectMocks
+    private ValidateMitigationJourneyHandler validateMitigationJourneyHandler =
+            new ValidateMitigationJourneyHandler();
+
+    @Test
+    void shouldReturnHelloWorld() {
+        APIGatewayProxyRequestEvent event = new APIGatewayProxyRequestEvent();
+
+        APIGatewayProxyResponseEvent response =
+                validateMitigationJourneyHandler.handleRequest(event, mockContext);
+
+        assertEquals("\"hello world\"", response.getBody());
+    }
+}
