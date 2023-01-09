7377de11286072ea619603c3cbfcd7d5a185c482
Daniel Pomfret <daniel.pomfret@digital.cabinet-office.gov.uk>
2023-01-09 15:53:42 +0000

    BUA: Address comment on snake_case for json.

--- lib/src/main/java/uk/gov/di/ipv/core/library/dto/CredentialIssuerErrorDto.java
+++ lib/src/main/java/uk/gov/di/ipv/core/library/dto/CredentialIssuerErrorDto.java
@@ -1,5 +1,6 @@
 package uk.gov.di.ipv.core.library.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import uk.gov.di.ipv.core.library.annotations.ExcludeFromGeneratedCoverageReport;
@@ -8,10 +9,21 @@ import uk.gov.di.ipv.core.library.annotations.ExcludeFromGeneratedCoverageReport
 @Getter
 @AllArgsConstructor
 public class CredentialIssuerErrorDto {
+    @JsonProperty("error")
     private final String error;
+
+    @JsonProperty(value = "error_description")
     private final String errorDescription;
+
+    @JsonProperty(value = "state")
     private final String state;
+
+    @JsonProperty(value = "credential_issuer_id")
     private final String credentialIssuerId;
+
+    @JsonProperty(value = "ipv_session_id")
     private final String ipvSessionId;
+
+    @JsonProperty(value = "redirect_uri")
     private final String redirectUri;
 }
