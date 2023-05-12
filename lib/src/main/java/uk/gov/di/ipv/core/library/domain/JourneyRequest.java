f615ff88717683bf5778e80901294e77ad6104c5
Christopher Peerman <122806395+chris-dco@users.noreply.github.com>
2023-05-12 10:38:55 +0100

    PYIC-2739: Fixed spotless issues

--- lib/src/main/java/uk/gov/di/ipv/core/library/domain/JourneyRequest.java
+++ lib/src/main/java/uk/gov/di/ipv/core/library/domain/JourneyRequest.java
@@ -2,25 +2,20 @@ package uk.gov.di.ipv.core.library.domain;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import uk.gov.di.ipv.core.library.annotations.ExcludeFromGeneratedCoverageReport;
 
 @Getter
 @ExcludeFromGeneratedCoverageReport
 public class JourneyRequest {
-    @JsonProperty
-    private String ipvSessionId;
+    @JsonProperty private String ipvSessionId;
 
-    @JsonProperty
-    private String ipAddress;
+    @JsonProperty private String ipAddress;
 
     @JsonCreator
     public JourneyRequest(
             @JsonProperty(value = "ipvSessionId") String ipvSessionId,
-            @JsonProperty(value = "ipAddress") String ipAddress
-    ) {
+            @JsonProperty(value = "ipAddress") String ipAddress) {
         this.ipvSessionId = ipvSessionId;
         this.ipAddress = ipAddress;
     }
