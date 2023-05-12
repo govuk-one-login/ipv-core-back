4a6150207c24dd0dbfea28658f2dc8aa8469a04c
Christopher Peerman <122806395+chris-dco@users.noreply.github.com>
2023-05-12 10:31:16 +0100

    PYIC-2739: Added call to /journey/check-existing-identity through JSON request

--- lib/src/main/java/uk/gov/di/ipv/core/library/domain/JourneyRequest.java
+++ lib/src/main/java/uk/gov/di/ipv/core/library/domain/JourneyRequest.java
@@ -1,11 +1,27 @@
 package uk.gov.di.ipv.core.library.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import uk.gov.di.ipv.core.library.annotations.ExcludeFromGeneratedCoverageReport;
 
-@AllArgsConstructor
-@Data
+@Getter
+@ExcludeFromGeneratedCoverageReport
 public class JourneyRequest {
+    @JsonProperty
     private String ipvSessionId;
+
+    @JsonProperty
     private String ipAddress;
+
+    @JsonCreator
+    public JourneyRequest(
+            @JsonProperty(value = "ipvSessionId") String ipvSessionId,
+            @JsonProperty(value = "ipAddress") String ipAddress
+    ) {
+        this.ipvSessionId = ipvSessionId;
+        this.ipAddress = ipAddress;
+    }
 }
