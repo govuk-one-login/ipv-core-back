9a730dc0c7c485ca6643929aa1b3ed9cc75724a6
“Talat <talat.aydin@digital.cabinet-office.gov.uk>
2023-05-15 16:42:18 +0100

    fixed code smell minor

--- lambdas/build-cri-oauth-request/src/main/java/uk/gov/di/ipv/core/buildcrioauthrequest/domain/CriResponse.java
+++ lambdas/build-cri-oauth-request/src/main/java/uk/gov/di/ipv/core/buildcrioauthrequest/domain/CriResponse.java
@@ -2,10 +2,12 @@ package uk.gov.di.ipv.core.buildcrioauthrequest.domain;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import uk.gov.di.ipv.core.library.domain.JourneyResponse;
 
 @Getter
+@EqualsAndHashCode(callSuper = true)
 public class CriResponse extends JourneyResponse {
     @JsonProperty private final CriDetails cri;
 
