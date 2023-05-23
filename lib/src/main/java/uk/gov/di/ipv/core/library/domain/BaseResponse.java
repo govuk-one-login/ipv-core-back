3e6ddc833d53d8200ea716e1d4f8e2f8ccfa36ed
Rohit Payal <rohit.payal@digital.cabinet-office.gov.uk>
2023-05-23 11:26:44 +0100

    PYIC-2731 Applied spotless check.

--- lib/src/main/java/uk/gov/di/ipv/core/library/domain/BaseResponse.java
+++ lib/src/main/java/uk/gov/di/ipv/core/library/domain/BaseResponse.java
@@ -2,20 +2,11 @@ package uk.gov.di.ipv.core.library.domain;
 
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import uk.gov.di.ipv.core.library.domain.buildclientoauthresponse.ClientResponse;
-
-import java.util.Map;
 
 import static com.fasterxml.jackson.annotation.JsonTypeInfo.As.PROPERTY;
 import static com.fasterxml.jackson.annotation.JsonTypeInfo.Id.NAME;
 
 @JsonTypeInfo(use = NAME, include = PROPERTY)
 @JsonSubTypes({@JsonSubTypes.Type(value = ClientResponse.class, name = "client")})
-public interface BaseResponse {
-    default Map<String, Object> toMap() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        return objectMapper.convertValue(this, new TypeReference<Map<String, Object>>() {});
-    }
-}
+public interface BaseResponse {}
