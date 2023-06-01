801332e1a6ffb7c10a19143fba569ce9fb3535e6
Martin Slade <martin.slade@digital.cabinet-office.gov.uk>
2023-06-01 11:10:31 +0100

    PYIC-2843: Add the async cri response queue mapping and the async cri response lambda

--- lambdas/process-async-cri-credential/src/main/java/uk/gov/di/ipv/core/processasynccricredential/ProcessAsyncCriCredentialHandler.java
+++ lambdas/process-async-cri-credential/src/main/java/uk/gov/di/ipv/core/processasynccricredential/ProcessAsyncCriCredentialHandler.java
@@ -2,66 +2,54 @@ package uk.gov.di.ipv.core.processasynccricredential;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SQSBatchResponse;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent.SQSMessage;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import software.amazon.lambda.powertools.logging.Logging;
 import software.amazon.lambda.powertools.tracing.Tracing;
 import uk.gov.di.ipv.core.library.annotations.ExcludeFromGeneratedCoverageReport;
-import uk.gov.di.ipv.core.library.service.AuditService;
-import uk.gov.di.ipv.core.library.service.CiStorageService;
 import uk.gov.di.ipv.core.library.service.ConfigService;
-import uk.gov.di.ipv.core.library.verifiablecredential.service.VerifiableCredentialService;
-import uk.gov.di.ipv.core.library.verifiablecredential.validation.VerifiableCredentialJwtValidator;
+import uk.gov.di.ipv.core.processasynccricredential.dto.CriResponseMessage;
 
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProcessAsyncCriCredentialHandler
-        implements RequestHandler<SQSEvent, Map<String, Object>> {
-    private static final String EVIDENCE = "evidence";
+        implements RequestHandler<SQSEvent, SQSBatchResponse> {
     private static final Logger LOGGER = LogManager.getLogger();
-    private final VerifiableCredentialService verifiableCredentialService;
+    private static final ObjectMapper mapper = new ObjectMapper();
     private final ConfigService configService;
-    private final AuditService auditService;
-    private final VerifiableCredentialJwtValidator verifiableCredentialJwtValidator;
-    private final CiStorageService ciStorageService;
 
-    private String componentId;
-
-    public ProcessAsyncCriCredentialHandler(
-            VerifiableCredentialService verifiableCredentialService,
-            ConfigService configService,
-            AuditService auditService,
-            VerifiableCredentialJwtValidator verifiableCredentialJwtValidator,
-            CiStorageService ciStorageService) {
-        this.verifiableCredentialService = verifiableCredentialService;
+    public ProcessAsyncCriCredentialHandler(ConfigService configService) {
         this.configService = configService;
-        this.auditService = auditService;
-        this.verifiableCredentialJwtValidator = verifiableCredentialJwtValidator;
-        this.ciStorageService = ciStorageService;
     }
 
     @ExcludeFromGeneratedCoverageReport
     public ProcessAsyncCriCredentialHandler() {
         this.configService = new ConfigService();
-        this.verifiableCredentialService = new VerifiableCredentialService(configService);
-        this.auditService = new AuditService(AuditService.getDefaultSqsClient(), configService);
-        this.verifiableCredentialJwtValidator = new VerifiableCredentialJwtValidator();
-        this.ciStorageService = new CiStorageService(configService);
     }
 
     @Override
     @Tracing
     @Logging(clearState = true)
-    public Map<String, Object> handleRequest(SQSEvent event, Context context) {
+    public SQSBatchResponse handleRequest(SQSEvent event, Context context) {
+        List<SQSBatchResponse.BatchItemFailure> failedRecords = new ArrayList<>();
         LOGGER.log(Level.INFO, event);
         for (SQSMessage message : event.getRecords()) {
             LOGGER.log(Level.INFO, message);
+            try {
+                final CriResponseMessage criResponseMessage =
+                        mapper.readerFor(CriResponseMessage.class).readValue(message.getBody());
+            } catch (JsonProcessingException e) {
+                LOGGER.error("Failed to deserialise message");
+                failedRecords.add(new SQSBatchResponse.BatchItemFailure(message.getMessageId()));
+            }
         }
-        return null;
-
+        return SQSBatchResponse.builder().withBatchItemFailures(failedRecords).build();
     }
-
 }
