7026c20e6fec931021be1776db3322486850c24e
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2023-06-26 12:25:58 +0100

    PYIC-3010: Load statemachines and select from session
    This adds logic to the process-journey-step state machine to load
    statemachine files for the journey types that we care about. This is now
    the main journey that users are currently running, as well as the
    refactor journey that we're building.
    
    The lambda will then use the state machine for the correct journey type,
    set in their session when it was initialized. If a matching state
    machine can't be found (which shouldn't ever happen), we log it and
    throw an error.

--- lib/src/main/java/uk/gov/di/ipv/core/library/domain/IpvJourneyTypes.java
+++ lib/src/main/java/uk/gov/di/ipv/core/library/domain/IpvJourneyTypes.java
@@ -1,7 +1,8 @@
 package uk.gov.di.ipv.core.library.domain;
 
 public enum IpvJourneyTypes {
-    IPV_CORE_MAIN_JOURNEY("ipv-core-main-journey");
+    IPV_CORE_MAIN_JOURNEY("ipv-core-main-journey"),
+    IPV_CORE_REFACTOR_JOURNEY("ipv-core-refactor-journey");
 
     private final String value;
 
