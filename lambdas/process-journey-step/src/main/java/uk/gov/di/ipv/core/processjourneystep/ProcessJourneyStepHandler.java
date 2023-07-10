ba2229b2fd94c17a06603b2dcfde83cc7e81bea3
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2023-07-10 10:29:42 +0100

    PYIC-3083: Load main journey in process journey step
    We're renaming the new journey back to the old name. I forgot to load
    the statemachine file. Facepalm.

--- lambdas/process-journey-step/src/main/java/uk/gov/di/ipv/core/processjourneystep/ProcessJourneyStepHandler.java
+++ lambdas/process-journey-step/src/main/java/uk/gov/di/ipv/core/processjourneystep/ProcessJourneyStepHandler.java
@@ -37,6 +37,7 @@ import java.util.List;
 import java.util.Map;
 
 import static uk.gov.di.ipv.core.library.config.ConfigurationVariable.BACKEND_SESSION_TIMEOUT;
+import static uk.gov.di.ipv.core.library.domain.IpvJourneyTypes.IPV_CORE_MAIN_JOURNEY;
 import static uk.gov.di.ipv.core.library.domain.IpvJourneyTypes.IPV_CORE_REFACTOR_JOURNEY;
 import static uk.gov.di.ipv.core.library.helpers.LogHelper.LogField.LOG_JOURNEY_STEP;
 import static uk.gov.di.ipv.core.library.helpers.LogHelper.LogField.LOG_JOURNEY_TYPE;
@@ -71,7 +72,8 @@ public class ProcessJourneyStepHandler
         this.configService = new ConfigService();
         this.ipvSessionService = new IpvSessionService(configService);
         this.clientOAuthSessionService = new ClientOAuthSessionDetailsService(configService);
-        this.stateMachines = loadStateMachines(List.of(IPV_CORE_REFACTOR_JOURNEY));
+        this.stateMachines =
+                loadStateMachines(List.of(IPV_CORE_REFACTOR_JOURNEY, IPV_CORE_MAIN_JOURNEY));
     }
 
     @Override
