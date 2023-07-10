427f8dc6ed38be7e04383e06b22a3e66c0235faf
Patrick Blakey <patrick.blakey@digital.cabinet-office.gov.uk>
2023-07-10 16:14:31 +0100

    PYIC-3167 Handle CRI disabled switch for basic state machine event

--- lambdas/process-journey-step/src/test/java/uk/gov/di/ipv/core/processjourneystep/statemachine/StateTest.java
+++ lambdas/process-journey-step/src/test/java/uk/gov/di/ipv/core/processjourneystep/statemachine/StateTest.java
@@ -1,27 +1,33 @@
 package uk.gov.di.ipv.core.processjourneystep.statemachine;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.di.ipv.core.library.service.ConfigService;
 import uk.gov.di.ipv.core.processjourneystep.statemachine.events.BasicEvent;
 import uk.gov.di.ipv.core.processjourneystep.statemachine.exceptions.UnknownEventException;
 import uk.gov.di.ipv.core.processjourneystep.statemachine.responses.JourneyContext;
 import uk.gov.di.ipv.core.processjourneystep.statemachine.responses.JourneyResponse;
+import uk.org.webcompere.systemstubs.jupiter.SystemStubsExtension;
 
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+@ExtendWith(MockitoExtension.class)
+@ExtendWith(SystemStubsExtension.class)
 public class StateTest {
-
+    @Mock private static ConfigService mockConfigService;
     public static final State CURRENT_STATE = new State("CURRENT_STATE");
     private static final State TARGET_STATE = new State("TARGET_STATE");
     private static final JourneyResponse JOURNEY_RESPONSE = new JourneyResponse("stepId");
-    private static final BasicEvent CURRENT_TO_TARGET_EVENT = new BasicEvent();
-    ;
 
-    @BeforeAll
-    private static void beforeAll() {
+    @BeforeEach
+    private void beforeEach() {
+        BasicEvent CURRENT_TO_TARGET_EVENT = new BasicEvent(mockConfigService);
         CURRENT_TO_TARGET_EVENT.setName("eventName");
         CURRENT_TO_TARGET_EVENT.setTargetState(TARGET_STATE);
         CURRENT_TO_TARGET_EVENT.setResponse(JOURNEY_RESPONSE);
