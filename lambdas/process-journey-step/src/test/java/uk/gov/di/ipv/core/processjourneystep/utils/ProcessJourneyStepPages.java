883e23c35e1a405da463c7fbf835d34e38dd3405
Shivanshu Patel <shivanshu.patel@digital.cabinet-office.gov.uk>
2023-07-11 14:40:16 +0100

    PYIC-3074: replance start page with new ipv start page

--- lambdas/process-journey-step/src/test/java/uk/gov/di/ipv/core/processjourneystep/utils/ProcessJourneyStepPages.java
+++ lambdas/process-journey-step/src/test/java/uk/gov/di/ipv/core/processjourneystep/utils/ProcessJourneyStepPages.java
@@ -2,7 +2,7 @@ package uk.gov.di.ipv.core.processjourneystep.utils;
 
 public class ProcessJourneyStepPages {
     public static final String IPV_IDENTITY_REUSE_PAGE = "page-ipv-reuse";
-    public static final String IPV_IDENTITY_START_PAGE = "page-ipv-identity-start";
+    public static final String IPV_IDENTITY_START_PAGE = "page-ipv-identity-document-start";
     public static final String PRE_KBV_TRANSITION_PAGE = "page-pre-kbv-transition";
     public static final String PYI_NO_MATCH_PAGE = "pyi-no-match";
     public static final String PYI_TECHNICAL_ERROR_PAGE = "pyi-technical";
