5ea733cd6056dca9e142e30a56ac381662993bc1
Rohit Payal <rohit.payal@digital.cabinet-office.gov.uk>
2023-09-04 14:19:14 +0100

    PYIC-3484 Added more tests for sonar coverage.

--- libs/cimit-service/src/test/java/uk/gov/di/ipv/core/library/cimit/CiMitServiceTest.java
+++ libs/cimit-service/src/test/java/uk/gov/di/ipv/core/library/cimit/CiMitServiceTest.java
@@ -44,6 +44,7 @@ import static uk.gov.di.ipv.core.library.config.EnvironmentVariable.CI_STORAGE_G
 import static uk.gov.di.ipv.core.library.config.EnvironmentVariable.CI_STORAGE_POST_MITIGATIONS_LAMBDA_ARN;
 import static uk.gov.di.ipv.core.library.config.EnvironmentVariable.CI_STORAGE_PUT_LAMBDA_ARN;
 import static uk.gov.di.ipv.core.library.fixtures.TestFixtures.EC_PUBLIC_JWK;
+import static uk.gov.di.ipv.core.library.fixtures.TestFixtures.SIGNED_CONTRA_INDICATOR_NO_VC;
 import static uk.gov.di.ipv.core.library.fixtures.TestFixtures.SIGNED_CONTRA_INDICATOR_VC;
 import static uk.gov.di.ipv.core.library.fixtures.TestFixtures.SIGNED_CONTRA_INDICATOR_VC_INVALID_EVIDENCE;
 import static uk.gov.di.ipv.core.library.fixtures.TestFixtures.SIGNED_CONTRA_INDICATOR_VC_NO_EVIDENCE;
@@ -480,6 +481,49 @@ class CiMitServiceTest {
                 CiRetrievalException.class,
                 () ->
                         ciMitService.getContraIndicatorsVCJwt(
+                                TEST_USER_ID, GOVUK_SIGNIN_JOURNEY_ID, CLIENT_SOURCE_IP));
+    }
+
+    @Test
+    void getContraIndicatorsVCJwtWhenInvalidCimitKey() throws JsonProcessingException {
+        when(configService.getEnvironmentVariable(CIMIT_GET_CONTRAINDICATORS_LAMBDA_ARN))
+                .thenReturn(THE_ARN_OF_CIMIT_GET_CI_LAMBDA);
+        when(configService.getSsmParameter(ConfigurationVariable.CIMIT_COMPONENT_ID))
+                .thenReturn(CIMIT_COMPONENT_ID);
+        when(configService.getSsmParameter(ConfigurationVariable.CIMIT_SIGNING_KEY))
+                .thenReturn("INVALID_CIMIT_KEY");
+        when(lambdaClient.invoke(any()))
+                .thenReturn(
+                        new InvokeResult()
+                                .withStatusCode(200)
+                                .withPayload(makeCiMitVCPayload(SIGNED_CONTRA_INDICATOR_VC)));
+
+        assertThrows(
+                CiRetrievalException.class,
+                () ->
+                        ciMitService.getContraIndicatorsVCJwt(
+                                TEST_USER_ID, GOVUK_SIGNIN_JOURNEY_ID, CLIENT_SOURCE_IP));
+    }
+
+    @Test
+    void getContraIndicatorsVCJwtWhenNoVcBlock()
+            throws CiRetrievalException, JsonProcessingException {
+        when(configService.getEnvironmentVariable(CIMIT_GET_CONTRAINDICATORS_LAMBDA_ARN))
+                .thenReturn(THE_ARN_OF_CIMIT_GET_CI_LAMBDA);
+        when(configService.getSsmParameter(ConfigurationVariable.CIMIT_COMPONENT_ID))
+                .thenReturn(CIMIT_COMPONENT_ID);
+        when(configService.getSsmParameter(ConfigurationVariable.CIMIT_SIGNING_KEY))
+                .thenReturn(EC_PUBLIC_JWK);
+        when(lambdaClient.invoke(requestCaptor.capture()))
+                .thenReturn(
+                        new InvokeResult()
+                                .withStatusCode(200)
+                                .withPayload(makeCiMitVCPayload(SIGNED_CONTRA_INDICATOR_NO_VC)));
+
+        assertThrows(
+                CiRetrievalException.class,
+                () ->
+                        ciMitService.getContraIndicatorsVC(
                                 TEST_USER_ID, GOVUK_SIGNIN_JOURNEY_ID, CLIENT_SOURCE_IP));
     }
 
