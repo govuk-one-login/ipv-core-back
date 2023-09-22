26ab5b3782048261e34940cf07ec7ab234c2222f
Laurence Webb <laurence.webb@digital.cabinet-office.gov.uk>
2023-09-22 11:28:47 +0100

    PYIC-3541 Revert rename of CI_SCORING_THRESHOLD

--- libs/common-services/src/main/java/uk/gov/di/ipv/core/library/config/ConfigurationVariable.java
+++ libs/common-services/src/main/java/uk/gov/di/ipv/core/library/config/ConfigurationVariable.java
@@ -14,7 +14,7 @@ public enum ConfigurationVariable {
     PUBLIC_KEY_MATERIAL_FOR_CORE_TO_VERIFY("clients/%s/publicKeyMaterialForCoreToVerify"),
     CLIENT_VALID_REDIRECT_URLS("clients/%s/validRedirectUrls"),
     CI_SCORING_CONFIG("self/ci-scoring-config"),
-    CI_SCORING_THRESHOLD("self/checkCiScoreThreshold"),
+    CI_SCORING_THRESHOLD("self/ciScoringThreshold"),
     VC_TTL("self/vcTtl"),
     CREDENTIAL_ISSUERS("credentialIssuers"),
     FEATURE_FLAGS("featureFlags/%s"),
