9ba99709b54f8f801f21f06eaa4e343f93c0786b
TalatAydinGDS <126885991+TalatAydinGDS@users.noreply.github.com>
2023-09-26 11:26:32 +0100

    Revert "PYIC-3505: On-the-fly VC Status (4): Get status of last visited CRI VC on-the-fly in EvaluateGpg45ScoresHandler"

--- libs/user-identity-service/src/main/java/uk/gov/di/ipv/core/library/service/UserIdentityService.java
+++ libs/user-identity-service/src/main/java/uk/gov/di/ipv/core/library/service/UserIdentityService.java
@@ -172,16 +172,6 @@ public class UserIdentityService {
         return successfulVCStoreItems;
     }
 
-    public Optional<Boolean> getVCSuccessStatus(String userId, String criIssuer)
-            throws ParseException {
-        VcStoreItem vcStoreItem = getVcStoreItem(userId, criIssuer);
-        if (vcStoreItem != null) {
-            SignedJWT vc = SignedJWT.parse(vcStoreItem.getCredential());
-            return Optional.of(VcHelper.isSuccessfulVc(vc));
-        }
-        return Optional.empty();
-    }
-
     private JsonNode getVCClaimNode(String credential) throws HttpResponseExceptionWithErrorBody {
         try {
             return objectMapper
