c5c2f5e502ab54adbe4b1e35cb7e66b181afa933
mike.collingwood <mike.collingwood@digital.cabinet-office.gov.uk>
2023-10-02 12:36:24 +0100

    PYIC-3472: Increase coverage of CredentialEvidenceItemTest

--- libs/common-services/src/test/java/uk/gov/di/ipv/core/library/gpg45/domain/CredentialEvidenceItemTest.java
+++ libs/common-services/src/test/java/uk/gov/di/ipv/core/library/gpg45/domain/CredentialEvidenceItemTest.java
@@ -1,12 +1,15 @@
 package uk.gov.di.ipv.core.library.gpg45.domain;
 
 import org.junit.jupiter.api.Test;
+import uk.gov.di.ipv.core.library.exceptions.UnknownEvidenceTypeException;
 import uk.gov.di.ipv.core.library.gpg45.domain.CredentialEvidenceItem.EvidenceType;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CredentialEvidenceItemTest {
     @Test
@@ -57,11 +60,130 @@ class CredentialEvidenceItemTest {
     }
 
     @Test
-    void testGetEvidenceScore() {
+    void testGetTypeActivity() throws UnknownEvidenceTypeException {
         CredentialEvidenceItem credentialEvidenceItem =
-                new CredentialEvidenceItem(10, 20, Collections.emptyList());
-        assertEquals(10, credentialEvidenceItem.getStrengthScore());
-        assertEquals(20, credentialEvidenceItem.getValidityScore());
+                new CredentialEvidenceItem(EvidenceType.ACTIVITY, 30, Collections.emptyList());
+        assertEquals(EvidenceType.ACTIVITY, credentialEvidenceItem.getType());
+        assertEquals(30, credentialEvidenceItem.getActivityHistoryScore());
+    }
+
+    @Test
+    void testGetTypeIdentityFraud() throws UnknownEvidenceTypeException {
+        CredentialEvidenceItem credentialEvidenceItem =
+                new CredentialEvidenceItem(
+                        EvidenceType.IDENTITY_FRAUD, 30, Collections.emptyList());
+        assertEquals(EvidenceType.IDENTITY_FRAUD, credentialEvidenceItem.getType());
+        assertEquals(30, credentialEvidenceItem.getIdentityFraudScore());
+    }
+
+    @Test
+    void testGetTypeVerification() throws UnknownEvidenceTypeException {
+        CredentialEvidenceItem credentialEvidenceItem =
+                new CredentialEvidenceItem(EvidenceType.VERIFICATION, 30, Collections.emptyList());
+        assertEquals(EvidenceType.VERIFICATION, credentialEvidenceItem.getType());
+        assertEquals(30, credentialEvidenceItem.getVerificationScore());
+    }
+
+    @Test
+    void testGetTypeEvidence() throws UnknownEvidenceTypeException {
+        CredentialEvidenceItem credentialEvidenceItem =
+                CredentialEvidenceItem.builder().strengthScore(30).validityScore(30).build();
+        assertEquals(EvidenceType.EVIDENCE, credentialEvidenceItem.getType());
+    }
+
+    @Test
+    void testIsDcmaw() throws UnknownEvidenceTypeException {
+        CredentialEvidenceItem credentialEvidenceItem1 =
+                CredentialEvidenceItem.builder()
+                        .strengthScore(30)
+                        .validityScore(30)
+                        .checkDetails(List.of(new CheckDetail()))
+                        .failedCheckDetails(List.of(new CheckDetail()))
+                        .build();
+        assertEquals(EvidenceType.DCMAW, credentialEvidenceItem1.getType());
+
+        CredentialEvidenceItem credentialEvidenceItem2 =
+                CredentialEvidenceItem.builder()
+                        .strengthScore(30)
+                        .validityScore(30)
+                        .failedCheckDetails(List.of(new CheckDetail()))
+                        .build();
+        assertEquals(EvidenceType.DCMAW, credentialEvidenceItem2.getType());
+
+        CredentialEvidenceItem credentialEvidenceItem3 =
+                CredentialEvidenceItem.builder()
+                        .strengthScore(30)
+                        .validityScore(30)
+                        .checkDetails(List.of(new CheckDetail()))
+                        .build();
+        assertEquals(EvidenceType.DCMAW, credentialEvidenceItem3.getType());
+    }
+
+    @Test
+    void testIsF2F() throws UnknownEvidenceTypeException {
+        CredentialEvidenceItem credentialEvidenceItem1 =
+                CredentialEvidenceItem.builder()
+                        .strengthScore(30)
+                        .validityScore(30)
+                        .verificationScore(30)
+                        .checkDetails(List.of(new CheckDetail()))
+                        .failedCheckDetails(List.of(new CheckDetail()))
+                        .build();
+        assertEquals(EvidenceType.F2F, credentialEvidenceItem1.getType());
+
+        CredentialEvidenceItem credentialEvidenceItem2 =
+                CredentialEvidenceItem.builder()
+                        .strengthScore(30)
+                        .validityScore(30)
+                        .verificationScore(30)
+                        .checkDetails(List.of(new CheckDetail()))
+                        .build();
+        assertEquals(EvidenceType.F2F, credentialEvidenceItem2.getType());
+
+        CredentialEvidenceItem credentialEvidenceItem3 =
+                CredentialEvidenceItem.builder()
+                        .strengthScore(30)
+                        .validityScore(30)
+                        .verificationScore(30)
+                        .failedCheckDetails(List.of(new CheckDetail()))
+                        .build();
+        assertEquals(EvidenceType.F2F, credentialEvidenceItem3.getType());
+    }
+
+    @Test
+    void testIsFraudWithActivity() throws UnknownEvidenceTypeException {
+        CredentialEvidenceItem credentialEvidenceItem =
+                CredentialEvidenceItem.builder()
+                        .identityFraudScore(30)
+                        .activityHistoryScore(30)
+                        .build();
+        assertEquals(EvidenceType.FRAUD_WITH_ACTIVITY, credentialEvidenceItem.getType());
+    }
+
+    @Test
+    void testIsNino() throws UnknownEvidenceTypeException {
+        CredentialEvidenceItem credentialEvidenceItem1 =
+                CredentialEvidenceItem.builder()
+                        .checkDetails(List.of(new CheckDetail()))
+                        .failedCheckDetails(List.of(new CheckDetail()))
+                        .build();
+        assertEquals(EvidenceType.NINO, credentialEvidenceItem1.getType());
+
+        CredentialEvidenceItem credentialEvidenceItem2 =
+                CredentialEvidenceItem.builder()
+                        .failedCheckDetails(List.of(new CheckDetail()))
+                        .build();
+        assertEquals(EvidenceType.NINO, credentialEvidenceItem2.getType());
+
+        CredentialEvidenceItem credentialEvidenceItem3 =
+                CredentialEvidenceItem.builder().checkDetails(List.of(new CheckDetail())).build();
+        assertEquals(EvidenceType.NINO, credentialEvidenceItem3.getType());
+    }
+
+    @Test
+    void testGetTypeUnknown() {
+        CredentialEvidenceItem credentialEvidenceItem = CredentialEvidenceItem.builder().build();
+        assertThrows(UnknownEvidenceTypeException.class, credentialEvidenceItem::getType);
     }
 
     @Test
