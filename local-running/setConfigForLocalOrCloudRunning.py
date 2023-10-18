568356913268ef014e81fd91169c570281448a84
Dan Corder <142887905+DanCorderIPV@users.noreply.github.com>
2023-10-18 15:06:52 +0100

    PYIC-3730: Fix CRI type and port numbers
    Co-authored-by: Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>

--- local-running/setConfigForLocalOrCloudRunning.py
+++ local-running/setConfigForLocalOrCloudRunning.py
@@ -129,10 +129,10 @@ def get_local_running_params(environment, dev_account):
         Param(f"/{environment}/core/credentialIssuers/hmrcKbv/connections/local/componentId", "https://hmrc-kbv-cri.stubs.account.gov.uk"),
         Param(f"/{environment}/core/credentialIssuers/hmrcKbv/connections/local/requiresApiKey", "false"),
 
-        Param(f"/{environment}/core/credentialIssuers/bav/connections/local/authorizeUrl", "http://localhost:3012/authorize"),
+        Param(f"/{environment}/core/credentialIssuers/bav/connections/local/authorizeUrl", "http://localhost:3013/authorize"),
         Param(f"/{environment}/core/credentialIssuers/bav/connections/local/clientCallbackUrl", "http://localhost:3001/credential-issuer/callback/bav"),
-        Param(f"/{environment}/core/credentialIssuers/bav/connections/local/tokenUrl", "http://host.docker.internal:3012/token"),
-        Param(f"/{environment}/core/credentialIssuers/bav/connections/local/credentialUrl", "http://host.docker.internal:3012/credentials/issue"),
+        Param(f"/{environment}/core/credentialIssuers/bav/connections/local/tokenUrl", "http://host.docker.internal:3013/token"),
+        Param(f"/{environment}/core/credentialIssuers/bav/connections/local/credentialUrl", "http://host.docker.internal:3013/credentials/issue"),
         Param(f"/{environment}/core/credentialIssuers/bav/connections/local/clientId", f"ipv-core-dev{dev_account}"),
         Param(f"/{environment}/core/credentialIssuers/bav/connections/local/signingKey", "{\"kty\":\"EC\",\"crv\":\"P-256\",\"x\":\"RBXnILIdExUEWUJMlYeD6agE8u9gGgA3InKrd5TKhhY\",\"y\":\"kKtt9v_xq9oqvv5_E8AHcV77IYQfyNwaTQyTYxdO_UM\"}"),
         Param(f"/{environment}/core/credentialIssuers/bav/connections/local/encryptionKey", "{\"kty\":\"RSA\",\"e\":\"AQAB\",\"n\":\"vyapkvJXLwpYRJjbkQD99V2gcPEUKrO3dwjcAA9TPkLucQEZvYZvb7-wfSHxlvJlJcdS20r5PKKmqdPeW3Y4ir3WsVVeiht2iOZUreUO5O3V3o7ImvEjPS_2_ZKMHCwUf51a6WGOaDjO87OX_bluV2dp01n-E3kiIl6RmWCVywjn13fX3jsX0LMCM_bt3HofJqiYhhNymEwh39oR_D7EE5sLUii2XvpTYPa6L_uPwdKa4vRl4h4owrWEJaJifMorGcvqhCK1JOHqgknN_3cb_ns9Px6ynQCeFXvBDJy4q71clkBq_EZs5227Y1S222wXIwUYN8w5YORQe3M-pCIh1Q\"}"),
