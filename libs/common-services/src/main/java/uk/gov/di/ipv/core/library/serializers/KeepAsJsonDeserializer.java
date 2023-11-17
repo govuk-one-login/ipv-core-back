82e77edb48a3b7ad12b366b9805d31adf07ef0eb
Laurence Webb <laurence.webb@digital.cabinet-office.gov.uk>
2023-11-17 14:16:02 +0000

    PYIC-2892 More backwards compatibility

--- libs/common-services/src/main/java/uk/gov/di/ipv/core/library/serializers/KeepAsJsonDeserializer.java
+++ libs/common-services/src/main/java/uk/gov/di/ipv/core/library/serializers/KeepAsJsonDeserializer.java
@@ -1,7 +1,6 @@
 package uk.gov.di.ipv.core.library.serializers;
 
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.TreeNode;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 
@@ -13,7 +12,14 @@ public class KeepAsJsonDeserializer extends JsonDeserializer<String> {
     @Override
     public String deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
 
-        TreeNode tree = jp.getCodec().readTree(jp);
-        return tree.toString();
+        String tree = jp.getCodec().readTree(jp).toString();
+        // Escape double quotes and backslash for backwards compatibility with old parameters
+        if (tree != null
+                && tree.length() >= 2
+                && tree.charAt(0) == '\"'
+                && tree.charAt(tree.length() - 1) == '\"') {
+            return tree.substring(1, tree.length() - 1).replace("\\", "");
+        }
+        return tree;
     }
 }
