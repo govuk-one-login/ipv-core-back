e92b4cb568bf9a659128ea175217084cb8b0f926
“Talat <talat.aydin@digital.cabinet-office.gov.uk>
2023-11-17 16:33:42 +0000

    PYIC-4007: If the 'vtr' value is empty, a bad request will be returned

--- lambdas/initialise-ipv-session/src/main/java/uk/gov/di/ipv/core/initialiseipvsession/InitialiseIpvSessionHandler.java
+++ lambdas/initialise-ipv-session/src/main/java/uk/gov/di/ipv/core/initialiseipvsession/InitialiseIpvSessionHandler.java
@@ -136,7 +136,6 @@ public class InitialiseIpvSessionHandler
                                         LOG_MESSAGE_DESCRIPTION.getFieldName(),
                                         ErrorResponse.MISSING_VTR.getMessage());
                 LOGGER.error(message);
-
                 return ApiGatewayResponseGenerator.proxyJsonResponse(
                         HttpStatus.SC_BAD_REQUEST, ErrorResponse.MISSING_VTR);
             }
