066bd309e45a6da7bdd23839c61d8d528d5aa0bb
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2023-12-07 17:14:49 +0000

    PYIC-4167: Clean up exitCode cruft
    This removes the last remnants of the exitCodes.
    
    The ContraIndicatorConfig no longer needs to ignore unknown properties,
    as the exitCode property has been removed from the config.
    
    Also, we can update some test data.

--- libs/common-services/src/main/java/uk/gov/di/ipv/core/library/domain/ContraIndicatorConfig.java
+++ libs/common-services/src/main/java/uk/gov/di/ipv/core/library/domain/ContraIndicatorConfig.java
@@ -1,6 +1,5 @@
 package uk.gov.di.ipv.core.library.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,7 +7,6 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class ContraIndicatorConfig {
     private String ci;
     private Integer detectedScore;
