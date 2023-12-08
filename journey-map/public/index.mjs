a9b47b66c3cbd525c25bfe6c492addac65dd76d8
Joe Edwards <joe.edwards@digital.cabinet-office.gov.uk>
2023-12-08 11:23:40 +0000

    PYIC-3711: Update journey map copy to be more readable

--- journey-map/public/index.mjs
+++ journey-map/public/index.mjs
@@ -3,6 +3,21 @@ import svgPanZoom from 'https://cdn.jsdelivr.net/npm/svg-pan-zoom@3.6.1/+esm';
 import yaml from 'https://cdn.jsdelivr.net/npm/yaml@2.3.2/+esm';
 import { getOptions, render } from './render.mjs';
 
+const CRI_NAMES = {
+    address: 'Address CRI',
+    claimedIdentity: 'Claimed Identity CRI',
+    bav: 'Bank account CRI',
+    dcmaw: 'DCMAW (mobile app) CRI',
+    drivingLicence: 'Driving licence (web) CRI',
+    f2f: 'Face-to-face CRI',
+    fraud: 'Experian fraud CRI',
+    hmrcKbv: 'HMRC KBV CRI',
+    kbv: 'Experian KBV CRI',
+    nino: 'NINO CRI',
+    ukPassport: 'Passport (web) CRI',
+    ticf: 'TICF CRI',
+};
+
 mermaid.initialize({
     startOnLoad: false,
     // Required to enable links and callbacks
@@ -24,7 +39,7 @@ const journeyMap = yaml.parse(await journeyResponse.text());
 const nestedResponse = await fetch('./nested-journey-definitions.yaml');
 const nestedJourneys = yaml.parse(await nestedResponse.text());
 
-const setupOptions = (name, options, fieldset) => {
+const setupOptions = (name, options, fieldset, labels) => {
     if (options.length) {
         options.forEach((option) => {
             const input = document.createElement('input');
@@ -34,8 +49,11 @@ const setupOptions = (name, options, fieldset) => {
             input.id = option;
 
             const label = document.createElement('label');
-            label.innerText = option;
+            const span = document.createElement('span');
+            span.innerText = (labels && labels[option]) || option;
+
             label.appendChild(input);
+            label.appendChild(span);
 
             fieldset.appendChild(label);
         });
@@ -80,7 +98,7 @@ const setupMermaidClickHandlers = () => {
             case 'page':
                 return `Page node displaying the \'${def.pageId}\' screen in IPV Core.`;
             case 'cri':
-                return `CRI node routing to the '${def.criId}' CRI.`;
+                return `CRI node routing to the ${CRI_NAMES[def.criId] || `'${def.criId}' CRI`}.`;
             default:
                 return '';
         }
@@ -107,7 +125,7 @@ const setupMermaidClickHandlers = () => {
 
 const initialize = async () => {
     const { disabledOptions, featureFlagOptions } = getOptions(journeyMap);
-    setupOptions('disabledCri', disabledOptions, disabledInput);
+    setupOptions('disabledCri', disabledOptions, disabledInput, CRI_NAMES);
     setupOptions('featureFlag', featureFlagOptions, featureFlagInput);
     setupMermaidClickHandlers();
     form.addEventListener('change', async (event) => {
