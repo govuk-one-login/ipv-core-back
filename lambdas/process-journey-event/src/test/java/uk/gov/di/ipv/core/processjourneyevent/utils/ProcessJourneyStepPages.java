2d215f50d370bb0bdbb9d746bf1018bad1f6575f
Rohit Payal <rohit.payal@digital.cabinet-office.gov.uk>
2024-01-08 13:03:27 +0000

    PYIC-4273 Removed journey map state-UNEXPECTED_EVENT.

--- lambdas/process-journey-event/src/test/java/uk/gov/di/ipv/core/processjourneyevent/utils/ProcessJourneyStepPages.java
+++ lambdas/process-journey-event/src/test/java/uk/gov/di/ipv/core/processjourneyevent/utils/ProcessJourneyStepPages.java
@@ -1,10 +1,5 @@
 package uk.gov.di.ipv.core.processjourneyevent.utils;
 
 public class ProcessJourneyStepPages {
-    public static final String IPV_IDENTITY_REUSE_PAGE = "page-ipv-reuse";
-    public static final String IPV_IDENTITY_START_PAGE = "page-ipv-identity-document-start";
-    public static final String PRE_KBV_TRANSITION_PAGE = "page-pre-kbv-transition";
-    public static final String PYI_NO_MATCH_PAGE = "pyi-no-match";
-    public static final String PYI_TECHNICAL_ERROR_PAGE = "pyi-technical";
     public static final String PYI_UNRECOVERABLE_TIMEOUT_ERROR_PAGE = "pyi-timeout-unrecoverable";
 }
