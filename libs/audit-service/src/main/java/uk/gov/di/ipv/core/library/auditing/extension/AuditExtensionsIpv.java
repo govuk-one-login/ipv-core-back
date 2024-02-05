d6d702c28d5fb0da0809c5ca67d94d471345e2d6
Ibeh Sammy <samuel.ibeh@digital.cabinet-office.gov.uk>
2024-02-05 10:54:44 +0000

    PYIC-4248: Refactored AuditExtensionsIpv and added sendInheritedIdentityReceivedAuditEvent method

--- libs/audit-service/src/main/java/uk/gov/di/ipv/core/library/auditing/extension/AuditExtensionsIpv.java
+++ libs/audit-service/src/main/java/uk/gov/di/ipv/core/library/auditing/extension/AuditExtensionsIpv.java
@@ -1,26 +1,40 @@
 package uk.gov.di.ipv.core.library.auditing.extension;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import uk.gov.di.ipv.core.library.enums.Vot;
 
 import java.util.List;
 
-public class AuditExtensionsIpv extends AuditExtensionsVcEvidence {
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
+public class AuditExtensionsIpv implements AuditExtensions {
+
+    @JsonProperty("iss")
+    private final String iss;
+
+    @JsonProperty("evidence")
+    private final JsonNode evidence;
 
     @JsonProperty("vot")
-    private final String vot;
+    @JsonInclude(NON_NULL)
+    private final Vot vot;
 
     @JsonProperty("vtr")
+    @JsonInclude(NON_NULL)
     private final List<String> vtr;
 
     public AuditExtensionsIpv(
             @JsonProperty(value = "iss", required = false) String iss,
             @JsonProperty(value = "evidence", required = false) String evidence,
-            @JsonProperty(value = "successful", required = false) boolean successful,
-            @JsonProperty(value = "vot", required = false) String vot,
+            @JsonProperty(value = "vot", required = false) Vot vot,
             @JsonProperty(value = "vtr", required = false) List<String> vtr)
             throws JsonProcessingException {
-        super(iss, evidence, successful);
+        this.iss = iss;
+        this.evidence = evidence == null ? null : new ObjectMapper().readTree(evidence);
         this.vot = vot;
         this.vtr = vtr;
     }
