f36bb267a4a60b3e5ebe52907da00b59ef0c8333
Dan Corder <dan.corder@digital.cabinet-office.gov.uk>
2024-02-20 16:51:22 +0000

    PYIC-4110: Generating valid user identity - but doesn't match pact

--- libs/pact-test-helpers/src/main/java/uk/gov/di/ipv/core/library/pacttesthelpers/PactJwtBuilder.java
+++ libs/pact-test-helpers/src/main/java/uk/gov/di/ipv/core/library/pacttesthelpers/PactJwtBuilder.java
@@ -4,21 +4,23 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nimbusds.jose.util.Base64URL;
+import com.nimbusds.jwt.SignedJWT;
 import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.text.ParseException;
 
 public class PactJwtBuilder {
-    private final String minifiedHeaderJson;
-    private final String minifiedBodyJson;
-    private final String signature;
+    private final Base64URL base64Header;
+    private final Base64URL base64Body;
+    private final Base64URL base64Signature;
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
     public PactJwtBuilder(String headerJson, String bodyJson, String signature) {
-        this.minifiedHeaderJson = minifyJson(headerJson);
-        this.minifiedBodyJson = minifyJson(bodyJson);
-        this.signature = signature;
+        this.base64Header = Base64URL.encode(minifyJson(headerJson));
+        this.base64Body = Base64URL.encode(minifyJson(bodyJson));
+        this.base64Signature = new Base64URL(signature);
     }
 
     public static PactJwtBuilder fromPath(String path) throws IOException {
@@ -31,19 +33,15 @@ public class PactJwtBuilder {
     }
 
     public String buildJwt() {
-        return Base64URL.encode(minifiedHeaderJson)
-                + "."
-                + Base64URL.encode(minifiedBodyJson)
-                + "."
-                + signature;
+        return base64Header + "." + base64Body + "." + base64Signature;
+    }
+
+    public SignedJWT buildSignedJwt() throws ParseException {
+        return new SignedJWT(base64Header, base64Body, base64Signature);
     }
 
     public String buildRegexMatcherIgnoringSignature() {
-        return "^"
-                + Base64URL.encode(minifiedHeaderJson)
-                + "\\."
-                + Base64URL.encode(minifiedBodyJson)
-                + "\\..*";
+        return "^" + base64Header + "\\." + base64Body + "\\..*";
     }
 
     // This is test code and any exception we throw will be caught by the test framework and fail
