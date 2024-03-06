b12ab7b0d709be41a15585eeea72aad3e89d0759
Dan Corder <dan.corder@digital.cabinet-office.gov.uk>
2024-03-06 12:27:50 +0000

    BAU: Update CIC test

--- lambdas/process-cri-callback/src/test/java/uk/gov/di/ipv/core/processcricallback/pact/cicCri/ContractTest.java
+++ lambdas/process-cri-callback/src/test/java/uk/gov/di/ipv/core/processcricallback/pact/cicCri/ContractTest.java
@@ -484,8 +484,8 @@ class ContractTest {
             {
               "nbf": 4070908800,
               "iat": 4070908800,
-              "iss": "dummyClaimedIdentityComponentId",
               "jti": "jti",
+              "iss": "dummyClaimedIdentityComponentId",
               "sub": "test-subject",
               "vc": {
                 "@context": [
@@ -524,5 +524,5 @@ class ContractTest {
     // valid signature (using https://jwt.io works well) and record it here so the PACT file doesn't
     // change each time we run the tests.
     private static final String VALID_VC_SIGNATURE =
-            "eEofDmnpJ-KgbNOyZ-E_jXq2rU6XcDVYarKaPJebYdPFpKWJ-abSC8us9ZL7uHkaY8YwCkd6lAHMNDp6jdmy8A";
+            "vUTu7InEfr0t2VPglf1c7eacAYNGqxI5xkBhFWD5qu17-r2fG8SZKLzdLtX8njzw7TUI2U27DLbrwP6abnOEhQ";
 }
