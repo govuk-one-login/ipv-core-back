b8fa620a94f4a08d15a23ac4580c7911be04b61c
Dan Corder <dan.corder@digital.cabinet-office.gov.uk>
2024-03-26 16:21:55 +0000

    BAU: Fix broken signature in contract test

--- lambdas/process-cri-callback/src/test/java/uk/gov/di/ipv/core/processcricallback/pact/fraudCri/TokenTests.java
+++ lambdas/process-cri-callback/src/test/java/uk/gov/di/ipv/core/processcricallback/pact/fraudCri/TokenTests.java
@@ -262,5 +262,5 @@ class TokenTests {
             "eyJpc3MiOiJpcHYtY29yZSIsInN1YiI6Imlwdi1jb3JlIiwiYXVkIjoiZHVtbXlGcmF1ZENvbXBvbmVudElkIiwiZXhwIjo0MDcwOTA5NzAwLCJqdGkiOiJTY25GNGRHWHRoWllYU181azg1T2JFb1NVMDRXLUgzcWFfcDZucHYyWlVZIn0";
     // Signature generated using JWT.io
     private static final String CLIENT_ASSERTION_SIGNATURE =
-            "ZHj3_BoW2CJEsWA7i_Eq0tfi3HsmoHonHVmBaY7OAzOn-ZA_MYcIaRuMOCipK4_Ar1hCT1LMYveJrD0aYs1y0Q";
+            "LV74cQ6gFe5BjOmA8Xm43y1GIK7gQja66Ei355gpfPPJqsdUX1TvnyIhC3U3S1nW5bqV-kxqRuAbmfQb9N0R4w";
 }
