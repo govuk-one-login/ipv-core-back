ddd6a030601e0504ffed80f8dbf07015566d98e0
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2024-04-03 10:34:31 +0100

    PYIC-5685: Feature flag writes to session-credentials table
    We need sign off from security before we can start writing to this new
    table in prod. This puts the writes behind a flag which we'll be able to
    control in lower envs in the usual way.

--- libs/verifiable-credentials/src/test/java/uk/gov/di/ipv/core/library/verifiablecredential/service/SessionCredentialsServiceTest.java
+++ libs/verifiable-credentials/src/test/java/uk/gov/di/ipv/core/library/verifiablecredential/service/SessionCredentialsServiceTest.java
@@ -1,6 +1,8 @@
 package uk.gov.di.ipv.core.library.verifiablecredential.service;
 
 import com.nimbusds.oauth2.sdk.http.HTTPResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -13,6 +15,7 @@ import uk.gov.di.ipv.core.library.domain.VerifiableCredential;
 import uk.gov.di.ipv.core.library.exceptions.VerifiableCredentialException;
 import uk.gov.di.ipv.core.library.persistence.DataStore;
 import uk.gov.di.ipv.core.library.persistence.item.SessionCredentialItem;
+import uk.gov.di.ipv.core.library.service.ConfigService;
 
 import java.util.List;
 import java.util.stream.IntStream;
@@ -24,9 +27,11 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static uk.gov.di.ipv.core.library.config.CoreFeatureFlag.SESSION_CREDENTIALS_TABLE_WRITES;
 import static uk.gov.di.ipv.core.library.domain.ErrorResponse.FAILED_TO_DELETE_CREDENTIAL;
 import static uk.gov.di.ipv.core.library.fixtures.VcFixtures.vcDrivingPermit;
 import static uk.gov.di.ipv.core.library.fixtures.VcFixtures.vcNinoSuccessful;
@@ -38,82 +43,122 @@ class SessionCredentialsServiceTest {
     private static final VerifiableCredential CREDENTIAL_1 = vcDrivingPermit();
     private static final VerifiableCredential CREDENTIAL_2 = vcVerificationM1a();
     private static final VerifiableCredential CREDENTIAL_3 = vcNinoSuccessful();
+    @Mock private static ConfigService mockConfigService;
     @Captor private ArgumentCaptor<SessionCredentialItem> sessionCredentialItemArgumentCaptor;
     @Captor private ArgumentCaptor<String> ipvSessionIdArgumentCaptor;
     @Mock private DataStore<SessionCredentialItem> mockDataStore;
     @InjectMocks private SessionCredentialsService sessionCredentialService;
 
-    @Test
-    void persistCredentialShouldCreateItemInDataStore() throws Exception {
-        sessionCredentialService.persistCredential(CREDENTIAL_1, SESSION_ID, true);
+    @Nested
+    class WritesWithFeatureFlagDisabled {
+        @BeforeEach
+        void setUp() {
+            when(mockConfigService.enabled(SESSION_CREDENTIALS_TABLE_WRITES)).thenReturn(false);
+        }
 
-        verify(mockDataStore)
-                .create(
-                        sessionCredentialItemArgumentCaptor.capture(),
-                        eq(ConfigurationVariable.SESSION_CREDENTIALS_TTL));
-        var capturedSessionCredentialItem = sessionCredentialItemArgumentCaptor.getValue();
+        @Test
+        void persistCredentialShouldNotCreateItemInDataStore() throws Exception {
+            sessionCredentialService.persistCredential(CREDENTIAL_1, SESSION_ID, true);
 
-        assertEquals(SESSION_ID, capturedSessionCredentialItem.getIpvSessionId());
-        assertEquals(
-                String.format("drivingLicence#%s", CREDENTIAL_1.getSignedJwt().getSignature()),
-                capturedSessionCredentialItem.getSortKey());
-        assertEquals(CREDENTIAL_1.getVcString(), capturedSessionCredentialItem.getCredential());
-        assertTrue(capturedSessionCredentialItem.isReceivedThisSession());
+            verify(mockDataStore, never()).create(any(), any());
+        }
+
+        @Test
+        void persistCredentialsShouldNotCreateItemsInDataStore() throws Exception {
+            List<VerifiableCredential> credentialsToStore =
+                    List.of(CREDENTIAL_1, CREDENTIAL_2, CREDENTIAL_3);
+            sessionCredentialService.persistCredentials(credentialsToStore, SESSION_ID);
+
+            verify(mockDataStore, never()).create(any(), any());
+        }
     }
 
-    @Test
-    void persistCredentialShouldThrowVerifiableCredentialExceptionIfProblemStoring() {
-        doThrow(IllegalStateException.class).when(mockDataStore).create(any(), any());
+    @Nested
+    class WritesWithFeatureFlagEnabled {
+        @BeforeEach
+        void setUp() {
+            when(mockConfigService.enabled(SESSION_CREDENTIALS_TABLE_WRITES)).thenReturn(true);
+        }
 
-        assertThrows(
-                VerifiableCredentialException.class,
-                () -> sessionCredentialService.persistCredential(CREDENTIAL_1, SESSION_ID, false));
+        @Test
+        void persistCredentialShouldCreateItemInDataStore() throws Exception {
+            sessionCredentialService.persistCredential(CREDENTIAL_1, SESSION_ID, true);
+
+            verify(mockDataStore)
+                    .create(
+                            sessionCredentialItemArgumentCaptor.capture(),
+                            eq(ConfigurationVariable.SESSION_CREDENTIALS_TTL));
+            var capturedSessionCredentialItem = sessionCredentialItemArgumentCaptor.getValue();
+
+            assertEquals(SESSION_ID, capturedSessionCredentialItem.getIpvSessionId());
+            assertEquals(
+                    String.format("drivingLicence#%s", CREDENTIAL_1.getSignedJwt().getSignature()),
+                    capturedSessionCredentialItem.getSortKey());
+            assertEquals(CREDENTIAL_1.getVcString(), capturedSessionCredentialItem.getCredential());
+            assertTrue(capturedSessionCredentialItem.isReceivedThisSession());
+        }
+
+        @Test
+        void persistCredentialShouldThrowVerifiableCredentialExceptionIfProblemStoring() {
+            doThrow(IllegalStateException.class).when(mockDataStore).create(any(), any());
+
+            assertThrows(
+                    VerifiableCredentialException.class,
+                    () ->
+                            sessionCredentialService.persistCredential(
+                                    CREDENTIAL_1, SESSION_ID, false));
+        }
+
+        @Test
+        void persistCredentialsShouldStoreAllCredentials() throws Exception {
+            List<VerifiableCredential> credentialsToStore =
+                    List.of(CREDENTIAL_1, CREDENTIAL_2, CREDENTIAL_3);
+            sessionCredentialService.persistCredentials(credentialsToStore, SESSION_ID);
+
+            verify(mockDataStore, times(3))
+                    .create(
+                            sessionCredentialItemArgumentCaptor.capture(),
+                            eq(ConfigurationVariable.SESSION_CREDENTIALS_TTL));
+            List<SessionCredentialItem> createdItems =
+                    sessionCredentialItemArgumentCaptor.getAllValues();
+
+            IntStream.range(0, 3)
+                    .forEach(
+                            i -> {
+                                assertEquals(SESSION_ID, createdItems.get(i).getIpvSessionId());
+                                assertEquals(
+                                        credentialsToStore.get(i).getVcString(),
+                                        createdItems.get(i).getCredential());
+                                assertFalse(createdItems.get(i).isReceivedThisSession());
+                            });
+        }
     }
 
-    @Test
-    void persistCredentialsShouldStoreAllCredentials() throws Exception {
-        List<VerifiableCredential> credentialsToStore =
-                List.of(CREDENTIAL_1, CREDENTIAL_2, CREDENTIAL_3);
-        sessionCredentialService.persistCredentials(credentialsToStore, SESSION_ID);
+    @Nested
+    class Deletes {
+        @Test
+        void deleteSessionCredentialsShouldClearAllCredentialsForCurrentSession() throws Exception {
+            sessionCredentialService.deleteSessionCredentials(SESSION_ID);
 
-        verify(mockDataStore, times(3))
-                .create(
-                        sessionCredentialItemArgumentCaptor.capture(),
-                        eq(ConfigurationVariable.SESSION_CREDENTIALS_TTL));
-        List<SessionCredentialItem> createdItems =
-                sessionCredentialItemArgumentCaptor.getAllValues();
+            verify(mockDataStore).delete(ipvSessionIdArgumentCaptor.capture());
 
-        IntStream.range(0, 3)
-                .forEach(
-                        i -> {
-                            assertEquals(SESSION_ID, createdItems.get(i).getIpvSessionId());
-                            assertEquals(
-                                    credentialsToStore.get(i).getVcString(),
-                                    createdItems.get(i).getCredential());
-                            assertFalse(createdItems.get(i).isReceivedThisSession());
-                        });
-    }
+            assertEquals(SESSION_ID, ipvSessionIdArgumentCaptor.getValue());
+        }
 
-    @Test
-    void deleteSessionCredentialsShouldClearAllCredentialsForCurrentSession() throws Exception {
-        sessionCredentialService.deleteSessionCredentials(SESSION_ID);
+        @Test
+        void deleteSessionCredentialShouldThrowVerifiableCredentialExceptionIfProblemDeleting() {
+            when(mockDataStore.delete(SESSION_ID)).thenThrow(new IllegalStateException());
 
-        verify(mockDataStore).delete(ipvSessionIdArgumentCaptor.capture());
+            var verifiableCredentialException =
+                    assertThrows(
+                            VerifiableCredentialException.class,
+                            () -> sessionCredentialService.deleteSessionCredentials(SESSION_ID));
 
-        assertEquals(SESSION_ID, ipvSessionIdArgumentCaptor.getValue());
-    }
-
-    @Test
-    void deleteSessionCredentialShouldThrowVerifiableCredentialExceptionIfProblemDeleting() {
-        when(mockDataStore.delete(SESSION_ID)).thenThrow(new IllegalStateException());
-
-        var verifiableCredentialException =
-                assertThrows(
-                        VerifiableCredentialException.class,
-                        () -> sessionCredentialService.deleteSessionCredentials(SESSION_ID));
-
-        assertEquals(
-                HTTPResponse.SC_SERVER_ERROR, verifiableCredentialException.getHttpStatusCode());
-        assertEquals(FAILED_TO_DELETE_CREDENTIAL, verifiableCredentialException.getErrorResponse());
+            assertEquals(
+                    HTTPResponse.SC_SERVER_ERROR,
+                    verifiableCredentialException.getHttpStatusCode());
+            assertEquals(
+                    FAILED_TO_DELETE_CREDENTIAL, verifiableCredentialException.getErrorResponse());
+        }
     }
 }
