248350929ad7de83d2c41e8d614786dca07d84c3
Joe Edwards <joe.edwards@digital.cabinet-office.gov.uk>
2024-04-05 17:49:11 +0100

    BAU: Separate Lambda and Step Function endpoints

---
+++ local-running/src/main/java/uk/gov/di/ipv/coreback/handlers/JourneyEngineHandler.java
@@ -0,0 +1,139 @@
+package uk.gov.di.ipv.coreback.handlers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import uk.gov.di.ipv.core.buildclientoauthresponse.BuildClientOauthResponseHandler;
+import uk.gov.di.ipv.core.buildcrioauthrequest.BuildCriOauthRequestHandler;
+import uk.gov.di.ipv.core.callticfcri.CallTicfCriHandler;
+import uk.gov.di.ipv.core.checkexistingidentity.CheckExistingIdentityHandler;
+import uk.gov.di.ipv.core.checkgpg45score.CheckGpg45ScoreHandler;
+import uk.gov.di.ipv.core.evaluategpg45scores.EvaluateGpg45ScoresHandler;
+import uk.gov.di.ipv.core.library.domain.JourneyRequest;
+import uk.gov.di.ipv.core.library.domain.ProcessRequest;
+import uk.gov.di.ipv.core.processjourneyevent.ProcessJourneyEventHandler;
+import uk.gov.di.ipv.core.resetidentity.ResetIdentityHandler;
+import uk.gov.di.ipv.coreback.domain.CoreContext;
+import uk.gov.di.ipv.coreback.exceptions.UnrecognisedJourneyException;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class JourneyEngineHandler {
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+    public static final CoreContext EMPTY_CONTEXT = new CoreContext();
+    public static final String APPLICATION_JSON = "application/json";
+
+    public static final String JOURNEY = "journey";
+    public static final String IPV_SESSION_ID = "ipv-session-id";
+    public static final String IP_ADDRESS = "ip-address";
+    public static final String CLIENT_SESSION_ID = "client-session-id";
+    public static final String FEATURE_SET = "feature-set";
+
+    private final ProcessJourneyEventHandler processJourneyEventHandler;
+    private final CheckExistingIdentityHandler checkExistingIdentityHandler;
+    private final ResetIdentityHandler resetIdentityHandler;
+    private final BuildCriOauthRequestHandler buildCriOauthRequestHandler;
+    private final BuildClientOauthResponseHandler buildClientOauthResponseHandler;
+    private final CheckGpg45ScoreHandler checkGpg45ScoreHandler;
+    private final EvaluateGpg45ScoresHandler evaluateGpg45ScoresHandler;
+    private final CallTicfCriHandler callTicfCriHandler;
+
+    public JourneyEngineHandler() throws IOException {
+        this.processJourneyEventHandler = new ProcessJourneyEventHandler();
+        this.checkExistingIdentityHandler = new CheckExistingIdentityHandler();
+        this.resetIdentityHandler = new ResetIdentityHandler();
+        this.buildCriOauthRequestHandler = new BuildCriOauthRequestHandler();
+        this.buildClientOauthResponseHandler = new BuildClientOauthResponseHandler();
+        this.checkGpg45ScoreHandler = new CheckGpg45ScoreHandler();
+        this.evaluateGpg45ScoresHandler = new EvaluateGpg45ScoresHandler();
+        this.callTicfCriHandler = new CallTicfCriHandler();
+    }
+
+    private final Route journeyEngine =
+            (Request request, Response response) -> {
+                response.type(APPLICATION_JSON);
+                String journeyEvent = request.pathInfo();
+
+                while (true) {
+                    var processJourneyEventOutput = processJourneyEvent(request, journeyEvent);
+
+                    if (!processJourneyEventOutput.containsKey(JOURNEY)) {
+                        return OBJECT_MAPPER.writeValueAsString(processJourneyEventOutput);
+                    }
+
+                    var processJourneyStepOutput =
+                            processJourneyStep(request, processJourneyEventOutput);
+
+                    if (!processJourneyStepOutput.containsKey(JOURNEY)) {
+                        return OBJECT_MAPPER.writeValueAsString(processJourneyEventOutput);
+                    }
+
+                    journeyEvent = (String) processJourneyEventOutput.get(JOURNEY);
+                }
+            };
+
+    public Route getJourneyEngine() {
+        return journeyEngine;
+    }
+
+    // Corresponds to the ProcessJourneyStep state in the step function
+    private Map<String, Object> processJourneyEvent(Request request, String journeyEvent) {
+        return processJourneyEventHandler.handleRequest(
+                buildJourneyRequest(request, journeyEvent), EMPTY_CONTEXT);
+    }
+
+    // Corresponds to the ProcessJourneyStepResult state in the step function
+    private Map<String, Object> processJourneyStep(
+            Request request, Map<String, Object> processJourneyEventOutput) {
+        var journeyStep = (String) processJourneyEventOutput.get(JOURNEY);
+
+        return switch (journeyStep) {
+            case "/journey/check-existing-identity" -> checkExistingIdentityHandler.handleRequest(
+                    buildJourneyRequest(request, journeyStep), EMPTY_CONTEXT);
+            case "/journey/reset-identity" -> resetIdentityHandler.handleRequest(
+                    buildProcessRequest(request, processJourneyEventOutput), EMPTY_CONTEXT);
+            case "/journey/build-client-oauth-response" -> buildClientOauthResponseHandler
+                    .handleRequest(buildJourneyRequest(request, journeyStep), EMPTY_CONTEXT);
+            case "/journey/evaluate-gpg45-scores" -> evaluateGpg45ScoresHandler.handleRequest(
+                    buildProcessRequest(request, processJourneyEventOutput), EMPTY_CONTEXT);
+            case "/journey/check-gpg45-scores" -> checkGpg45ScoreHandler.handleRequest(
+                    buildProcessRequest(request, processJourneyEventOutput), EMPTY_CONTEXT);
+            case "/journey/call-ticf-cri" -> callTicfCriHandler.handleRequest(
+                    buildProcessRequest(request, processJourneyEventOutput), EMPTY_CONTEXT);
+            default -> {
+                if (journeyStep.matches("/journey/cri/build-oauth-request/.*")) {
+                    yield buildCriOauthRequestHandler.handleRequest(
+                            buildJourneyRequest(request, journeyStep), EMPTY_CONTEXT);
+                } else {
+                    throw new UnrecognisedJourneyException(
+                            String.format("Journey not configured: %s", journeyStep));
+                }
+            }
+        };
+    }
+
+    private JourneyRequest buildJourneyRequest(Request request, String journey) {
+        return JourneyRequest.builder()
+                .ipvSessionId(request.headers(IPV_SESSION_ID))
+                .ipAddress(request.headers(IP_ADDRESS))
+                .clientOAuthSessionId(request.headers(CLIENT_SESSION_ID))
+                .featureSet(request.headers(FEATURE_SET))
+                .journey(journey)
+                .build();
+    }
+
+    private ProcessRequest buildProcessRequest(
+            Request request, Map<String, Object> processJourneyEventOutput) {
+        return ProcessRequest.processRequestBuilder()
+                .ipvSessionId(request.headers(IPV_SESSION_ID))
+                .ipAddress(request.headers(IP_ADDRESS))
+                .clientOAuthSessionId(request.headers(CLIENT_SESSION_ID))
+                .featureSet(request.headers(FEATURE_SET))
+                .journey((String) processJourneyEventOutput.get(JOURNEY))
+                .lambdaInput((Map<String, Object>) processJourneyEventOutput.get("lambdaInput"))
+                .build();
+    }
+}
--- local-running/src/main/java/uk/gov/di/ipv/coreback/handlers/LambdaHandler.java
+++ local-running/src/main/java/uk/gov/di/ipv/coreback/handlers/LambdaHandler.java
@@ -3,133 +3,24 @@ package uk.gov.di.ipv.coreback.handlers;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 import spark.Request;
 import spark.Response;
 import spark.Route;
-import uk.gov.di.ipv.core.buildclientoauthresponse.BuildClientOauthResponseHandler;
-import uk.gov.di.ipv.core.buildcrioauthrequest.BuildCriOauthRequestHandler;
 import uk.gov.di.ipv.core.buildprovenuseridentitydetails.BuildProvenUserIdentityDetailsHandler;
 import uk.gov.di.ipv.core.builduseridentity.BuildUserIdentityHandler;
-import uk.gov.di.ipv.core.callticfcri.CallTicfCriHandler;
-import uk.gov.di.ipv.core.checkexistingidentity.CheckExistingIdentityHandler;
-import uk.gov.di.ipv.core.checkgpg45score.CheckGpg45ScoreHandler;
-import uk.gov.di.ipv.core.evaluategpg45scores.EvaluateGpg45ScoresHandler;
 import uk.gov.di.ipv.core.initialiseipvsession.InitialiseIpvSessionHandler;
 import uk.gov.di.ipv.core.issueclientaccesstoken.IssueClientAccessTokenHandler;
-import uk.gov.di.ipv.core.library.domain.JourneyRequest;
-import uk.gov.di.ipv.core.library.domain.ProcessRequest;
 import uk.gov.di.ipv.core.processcricallback.ProcessCriCallbackHandler;
-import uk.gov.di.ipv.core.processjourneyevent.ProcessJourneyEventHandler;
-import uk.gov.di.ipv.core.resetidentity.ResetIdentityHandler;
 import uk.gov.di.ipv.coreback.domain.CoreContext;
-import uk.gov.di.ipv.coreback.exceptions.UnrecognisedJourneyException;
 
-import java.io.IOException;
-import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.Map;
 
 public class LambdaHandler {
-
-    private static final Gson gson = new Gson();
     public static final CoreContext EMPTY_CONTEXT = new CoreContext();
-    public static final Type MAP_STRING_STRING_TYPE =
-            new TypeToken<Map<String, String>>() {}.getType();
     public static final String APPLICATION_JSON = "application/json";
-    public static final String JOURNEY = "journey";
-    public static final String IPV_SESSION_ID = "ipv-session-id";
-    public static final String IP_ADDRESS = "ip-address";
-    public static final String CLIENT_SESSION_ID = "client-session-id";
-    public static final String FEATURE_SET = "feature-set";
-
-    private ProcessJourneyEventHandler processJourneyEventHandler;
-    private CheckExistingIdentityHandler checkExistingIdentityHandler;
-    private ResetIdentityHandler resetIdentityHandler;
-    private BuildCriOauthRequestHandler buildCriOauthRequestHandler;
-    private BuildClientOauthResponseHandler buildClientOauthResponseHandler;
-    private CheckGpg45ScoreHandler checkGpg45ScoreHandler;
-    private EvaluateGpg45ScoresHandler evaluateGpg45ScoresHandler;
-    private CallTicfCriHandler callTicfCriHandler;
-
-    public LambdaHandler() throws IOException {
-        this.processJourneyEventHandler = new ProcessJourneyEventHandler();
-        this.checkExistingIdentityHandler = new CheckExistingIdentityHandler();
-        this.resetIdentityHandler = new ResetIdentityHandler();
-        this.buildCriOauthRequestHandler = new BuildCriOauthRequestHandler();
-        this.buildClientOauthResponseHandler = new BuildClientOauthResponseHandler();
-        this.checkGpg45ScoreHandler = new CheckGpg45ScoreHandler();
-        this.evaluateGpg45ScoresHandler = new EvaluateGpg45ScoresHandler();
-        this.callTicfCriHandler = new CallTicfCriHandler();
-    }
 
     private final Route initialiseSession = apiGatewayProxyRoute(new InitialiseIpvSessionHandler());
-
-    private final Route journeyEngine =
-            (Request request, Response response) -> {
-                String journey = request.pathInfo();
-
-                while (true) {
-                    var processJourneyEventOutput =
-                            processJourneyEventHandler.handleRequest(
-                                    buildProcessJourneyEventLambdaInput(request, journey),
-                                    EMPTY_CONTEXT);
-
-                    journey = (String) processJourneyEventOutput.get(JOURNEY);
-
-                    if (journey == null) {
-                        return gson.toJson(processJourneyEventOutput);
-                    }
-
-                    var lambdaOutput =
-                            switch (journey) {
-                                case "/journey/check-existing-identity" -> checkExistingIdentityHandler
-                                        .handleRequest(
-                                                buildJourneyRequest(request, journey),
-                                                EMPTY_CONTEXT);
-                                case "/journey/reset-identity" -> resetIdentityHandler
-                                        .handleRequest(
-                                                buildProcessRequest(
-                                                        request, processJourneyEventOutput),
-                                                EMPTY_CONTEXT);
-                                case "/journey/build-client-oauth-response" -> buildClientOauthResponseHandler
-                                        .handleRequest(
-                                                buildJourneyRequest(request, journey),
-                                                EMPTY_CONTEXT);
-                                case "/journey/evaluate-gpg45-scores" -> evaluateGpg45ScoresHandler
-                                        .handleRequest(
-                                                buildProcessRequest(
-                                                        request, processJourneyEventOutput),
-                                                EMPTY_CONTEXT);
-                                case "/journey/check-gpg45-scores" -> checkGpg45ScoreHandler
-                                        .handleRequest(
-                                                buildProcessRequest(
-                                                        request, processJourneyEventOutput),
-                                                EMPTY_CONTEXT);
-                                case "/journey/call-ticf-cri" -> callTicfCriHandler.handleRequest(
-                                        buildProcessRequest(request, processJourneyEventOutput),
-                                        EMPTY_CONTEXT);
-                                default -> {
-                                    if (journey.matches("/journey/cri/build-oauth-request/.*")) {
-                                        yield buildCriOauthRequestHandler.handleRequest(
-                                                buildJourneyRequest(request, journey),
-                                                EMPTY_CONTEXT);
-                                    } else {
-                                        throw new UnrecognisedJourneyException(
-                                                String.format(
-                                                        "Journey not configured: %s", journey));
-                                    }
-                                }
-                            };
-
-                    if (!lambdaOutput.containsKey(JOURNEY)) {
-                        return gson.toJson(lambdaOutput);
-                    }
-
-                    journey = (String) lambdaOutput.get(JOURNEY);
-                }
-            };
 
     private final Route buildProvenUserIdentityDetails =
             apiGatewayProxyRoute(new BuildProvenUserIdentityDetailsHandler());
@@ -156,16 +47,6 @@ public class LambdaHandler {
         };
     }
 
-    private JourneyRequest buildJourneyRequest(Request request, String journey) {
-        return JourneyRequest.builder()
-                .ipvSessionId(request.headers(IPV_SESSION_ID))
-                .ipAddress(request.headers(IP_ADDRESS))
-                .clientOAuthSessionId(request.headers(CLIENT_SESSION_ID))
-                .featureSet(request.headers(FEATURE_SET))
-                .journey(journey)
-                .build();
-    }
-
     private Map<String, String> getHeadersMap(Request request) {
         Map<String, String> headers = new HashMap<>();
         request.headers().forEach(header -> headers.put(header, request.headers(header)));
@@ -173,30 +54,8 @@ public class LambdaHandler {
         return headers;
     }
 
-    private Map<String, String> buildProcessJourneyEventLambdaInput(
-            Request request, String journey) {
-        return gson.fromJson(
-                gson.toJson(buildJourneyRequest(request, journey)), MAP_STRING_STRING_TYPE);
-    }
-
-    private ProcessRequest buildProcessRequest(
-            Request request, Map<String, Object> processJourneyEventOutput) {
-        return ProcessRequest.processRequestBuilder()
-                .ipvSessionId(request.headers(IPV_SESSION_ID))
-                .ipAddress(request.headers(IP_ADDRESS))
-                .clientOAuthSessionId(request.headers(CLIENT_SESSION_ID))
-                .featureSet(request.headers(FEATURE_SET))
-                .journey((String) processJourneyEventOutput.get(JOURNEY))
-                .lambdaInput((Map<String, Object>) processJourneyEventOutput.get("lambdaInput"))
-                .build();
-    }
-
     public Route getInitialiseSession() {
         return this.initialiseSession;
-    }
-
-    public Route getJourneyEngine() {
-        return this.journeyEngine;
     }
 
     public Route getBuildProvenUserIdentityDetails() {
