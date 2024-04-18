e20f2fdfab3e4910b292c6bf93df547eb8d515eb
Sammy Ibeh <samuel.ibeh@digital.cabinet-office.gov.uk>
2024-04-18 12:05:45 +0100

    PYIC-5597 - Implement logic for ResetIpvSessionHandler lambda
    Adds to logic for the new ResetIpvSession lambda
    
    Gets the session ID and clears the session credentials

--- lambdas/reset-ipv-session/src/main/java/uk/gov/di/ipv/core/resetipvsession/ResetIpvSessionHandler.java
+++ lambdas/reset-ipv-session/src/main/java/uk/gov/di/ipv/core/resetipvsession/ResetIpvSessionHandler.java
@@ -2,17 +2,76 @@ package uk.gov.di.ipv.core.resetipvsession;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import software.amazon.lambda.powertools.logging.Logging;
 import software.amazon.lambda.powertools.tracing.Tracing;
+import uk.gov.di.ipv.core.library.domain.JourneyErrorResponse;
+import uk.gov.di.ipv.core.library.domain.JourneyResponse;
 import uk.gov.di.ipv.core.library.domain.ProcessRequest;
+import uk.gov.di.ipv.core.library.exceptions.HttpResponseExceptionWithErrorBody;
+import uk.gov.di.ipv.core.library.exceptions.VerifiableCredentialException;
+import uk.gov.di.ipv.core.library.helpers.LogHelper;
+import uk.gov.di.ipv.core.library.helpers.RequestHelper;
+import uk.gov.di.ipv.core.library.persistence.item.ClientOAuthSessionItem;
+import uk.gov.di.ipv.core.library.persistence.item.IpvSessionItem;
+import uk.gov.di.ipv.core.library.service.ClientOAuthSessionDetailsService;
+import uk.gov.di.ipv.core.library.service.ConfigService;
+import uk.gov.di.ipv.core.library.service.IpvSessionService;
+import uk.gov.di.ipv.core.library.verifiablecredential.service.SessionCredentialsService;
 
 import java.util.Map;
 
+import static uk.gov.di.ipv.core.library.helpers.RequestHelper.getIpvSessionId;
+import static uk.gov.di.ipv.core.library.journeyuris.JourneyUris.JOURNEY_ERROR_PATH;
+import static uk.gov.di.ipv.core.library.journeyuris.JourneyUris.JOURNEY_NEXT_PATH;
+
 public class ResetIpvSessionHandler implements RequestHandler<ProcessRequest, Map<String, Object>> {
+    private static final Logger LOGGER = LogManager.getLogger();
+    private static final Map<String, Object> JOURNEY_NEXT =
+            new JourneyResponse(JOURNEY_NEXT_PATH).toObjectMap();
+    private final ConfigService configService;
+    private final IpvSessionService ipvSessionService;
+    private final SessionCredentialsService sessionCredentialsService;
+    private final ClientOAuthSessionDetailsService clientOAuthSessionDetailsService;
+
+    public ResetIpvSessionHandler(
+            ConfigService configService,
+            IpvSessionService ipvSessionService,
+            SessionCredentialsService sessionCredentialsService,
+            ClientOAuthSessionDetailsService clientOAuthSessionDetailsService) {
+        this.configService = configService;
+        this.ipvSessionService = ipvSessionService;
+        this.sessionCredentialsService = sessionCredentialsService;
+        this.clientOAuthSessionDetailsService = clientOAuthSessionDetailsService;
+    }
+
     @Override
     @Tracing
     @Logging(clearState = true)
     public Map<String, Object> handleRequest(ProcessRequest input, Context context) {
-        return null;
+        LogHelper.attachComponentId(configService);
+        configService.setFeatureSet(RequestHelper.getFeatureSet(input));
+        try {
+            String ipvSessionId = getIpvSessionId(input);
+            IpvSessionItem ipvSessionItem = ipvSessionService.getIpvSession(ipvSessionId);
+
+            ClientOAuthSessionItem clientOAuthSessionItem =
+                    clientOAuthSessionDetailsService.getClientOAuthSession(
+                            ipvSessionItem.getClientOAuthSessionId());
+            String govukSigninJourneyId = clientOAuthSessionItem.getGovukSigninJourneyId();
+            LogHelper.attachGovukSigninJourneyIdToLogs(govukSigninJourneyId);
+
+            sessionCredentialsService.deleteSessionCredentials(ipvSessionItem.getIpvSessionId());
+
+            LOGGER.info(LogHelper.buildLogMessage("Session credentials deleted"));
+
+            return JOURNEY_NEXT;
+        } catch (HttpResponseExceptionWithErrorBody | VerifiableCredentialException e) {
+            LOGGER.error(LogHelper.buildErrorMessage(e.getErrorResponse().getMessage(), e));
+            return new JourneyErrorResponse(
+                            JOURNEY_ERROR_PATH, e.getResponseCode(), e.getErrorResponse())
+                    .toObjectMap();
+        }
     }
 }
