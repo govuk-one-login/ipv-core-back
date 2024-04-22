77ee47450d79549600a8cf3c0aeca896c6febd44
Sam Barker <samuel.barker@digital.cabinet-office.gov.uk>
2024-04-22 11:05:22 +0100

    remove gson from testing

--- libs/common-services/src/main/java/uk/gov/di/ipv/core/library/domain/cimitvc/Mitigation.java
+++ libs/common-services/src/main/java/uk/gov/di/ipv/core/library/domain/cimitvc/Mitigation.java
@@ -1,12 +1,16 @@
 package uk.gov.di.ipv.core.library.domain.cimitvc;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 import uk.gov.di.ipv.core.library.annotations.ExcludeFromGeneratedCoverageReport;
 
 import java.util.List;
 
+@AllArgsConstructor
+@NoArgsConstructor(force = true)
 @Getter
 @Builder
 @ExcludeFromGeneratedCoverageReport
