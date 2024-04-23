a5b2b6294438a2cb4bd455c017ab43b4a6b4d5b2
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2024-04-23 10:28:40 +0100

    PYIC-5966: Send IPV_IDENTITY_STORED audit event
    This adds a new audit event for when we store an identity from the
    session credentials table into the long term storage solution.
    
    It includes an extension for the vot. We won't always have one however,
    specifically in the f2f journey. We store the partial identity before
    the f2f handoff page, so that the VCs are in the long term store ready
    for when the async VC arrives. In that scenario, we're setting the vot
    to null.

--- lambdas/store-identity/src/main/java/uk/gov/di/ipv/core/storeidentity/StoreIdentityHandler.java
+++ lambdas/store-identity/src/main/java/uk/gov/di/ipv/core/storeidentity/StoreIdentityHandler.java
@@ -7,14 +7,21 @@ import org.apache.logging.log4j.Logger;
 import software.amazon.lambda.powertools.logging.Logging;
 import software.amazon.lambda.powertools.tracing.Tracing;
 import uk.gov.di.ipv.core.library.annotations.ExcludeFromGeneratedCoverageReport;
+import uk.gov.di.ipv.core.library.auditing.AuditEvent;
+import uk.gov.di.ipv.core.library.auditing.AuditEventUser;
+import uk.gov.di.ipv.core.library.auditing.extension.AuditExtensionVot;
+import uk.gov.di.ipv.core.library.config.ConfigurationVariable;
 import uk.gov.di.ipv.core.library.domain.JourneyErrorResponse;
 import uk.gov.di.ipv.core.library.domain.JourneyResponse;
 import uk.gov.di.ipv.core.library.domain.ProcessRequest;
+import uk.gov.di.ipv.core.library.enums.Vot;
 import uk.gov.di.ipv.core.library.exceptions.HttpResponseExceptionWithErrorBody;
+import uk.gov.di.ipv.core.library.exceptions.SqsException;
 import uk.gov.di.ipv.core.library.exceptions.VerifiableCredentialException;
 import uk.gov.di.ipv.core.library.helpers.LogHelper;
 import uk.gov.di.ipv.core.library.helpers.RequestHelper;
 import uk.gov.di.ipv.core.library.persistence.item.ClientOAuthSessionItem;
+import uk.gov.di.ipv.core.library.service.AuditService;
 import uk.gov.di.ipv.core.library.service.ClientOAuthSessionDetailsService;
 import uk.gov.di.ipv.core.library.service.ConfigService;
 import uk.gov.di.ipv.core.library.service.IpvSessionService;
@@ -23,6 +30,10 @@ import uk.gov.di.ipv.core.library.verifiablecredential.service.VerifiableCredent
 
 import java.util.Map;
 
+import static com.nimbusds.oauth2.sdk.http.HTTPResponse.SC_SERVER_ERROR;
+import static uk.gov.di.ipv.core.library.auditing.AuditEventTypes.IPV_IDENTITY_STORED;
+import static uk.gov.di.ipv.core.library.domain.ErrorResponse.FAILED_TO_SEND_AUDIT_EVENT;
+import static uk.gov.di.ipv.core.library.enums.Vot.P0;
 import static uk.gov.di.ipv.core.library.journeyuris.JourneyUris.JOURNEY_ERROR_PATH;
 import static uk.gov.di.ipv.core.library.journeyuris.JourneyUris.JOURNEY_IDENTITY_STORED_PATH;
 
@@ -35,18 +46,21 @@ public class StoreIdentityHandler implements RequestHandler<ProcessRequest, Map<
     private final ClientOAuthSessionDetailsService clientOAuthSessionDetailsService;
     private final SessionCredentialsService sessionCredentialsService;
     private final VerifiableCredentialService verifiableCredentialService;
+    private final AuditService auditService;
 
     public StoreIdentityHandler(
             ConfigService configService,
             ClientOAuthSessionDetailsService clientOAuthSessionDetailsService,
             IpvSessionService ipvSessionService,
             SessionCredentialsService sessionCredentialsService,
-            VerifiableCredentialService verifiableCredentialService) {
+            VerifiableCredentialService verifiableCredentialService,
+            AuditService auditService) {
         this.configService = configService;
         this.clientOAuthSessionDetailsService = clientOAuthSessionDetailsService;
         this.ipvSessionService = ipvSessionService;
         this.sessionCredentialsService = sessionCredentialsService;
         this.verifiableCredentialService = verifiableCredentialService;
+        this.auditService = auditService;
     }
 
     @ExcludeFromGeneratedCoverageReport
@@ -56,6 +70,7 @@ public class StoreIdentityHandler implements RequestHandler<ProcessRequest, Map<
         this.clientOAuthSessionDetailsService = new ClientOAuthSessionDetailsService(configService);
         this.sessionCredentialsService = new SessionCredentialsService(configService);
         this.verifiableCredentialService = new VerifiableCredentialService(configService);
+        this.auditService = new AuditService(AuditService.getSqsClient(), configService);
     }
 
     @Override
@@ -80,6 +95,18 @@ public class StoreIdentityHandler implements RequestHandler<ProcessRequest, Map<
                     clientOAuthSessionItem.getUserId());
 
             LOGGER.info(LogHelper.buildLogMessage("Identity successfully stored"));
+            Vot vot = ipvSessionItem.getVot();
+
+            auditService.sendAuditEvent(
+                    new AuditEvent(
+                            IPV_IDENTITY_STORED,
+                            configService.getSsmParameter(ConfigurationVariable.COMPONENT_ID),
+                            new AuditEventUser(
+                                    clientOAuthSessionItem.getUserId(),
+                                    ipvSessionItem.getIpvSessionId(),
+                                    clientOAuthSessionItem.getGovukSigninJourneyId(),
+                                    input.getIpAddress()),
+                            new AuditExtensionVot(vot.equals(P0) ? null : vot)));
 
             return JOURNEY_IDENTITY_STORED;
 
@@ -87,6 +114,11 @@ public class StoreIdentityHandler implements RequestHandler<ProcessRequest, Map<
             LOGGER.error(LogHelper.buildErrorMessage("Failed to store identity", e));
             return new JourneyErrorResponse(
                             JOURNEY_ERROR_PATH, e.getResponseCode(), e.getErrorResponse())
+                    .toObjectMap();
+        } catch (SqsException e) {
+            LOGGER.error(LogHelper.buildErrorMessage("Failed to send audit event", e));
+            return new JourneyErrorResponse(
+                            JOURNEY_ERROR_PATH, SC_SERVER_ERROR, FAILED_TO_SEND_AUDIT_EVENT)
                     .toObjectMap();
         }
     }
