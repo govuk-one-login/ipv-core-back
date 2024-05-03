c6aafe5003bf11d8f13683f660f47aaa462e7af2
Sam Barker <samuel.barker@digital.cabinet-office.gov.uk>
2024-05-03 15:23:14 +0100

    use JsonIgnoreProperties on Journey

--- lambdas/process-journey-event/src/main/java/uk/gov/di/ipv/core/processjourneyevent/statemachine/StateMachineInitializer.java
+++ lambdas/process-journey-event/src/main/java/uk/gov/di/ipv/core/processjourneyevent/statemachine/StateMachineInitializer.java
@@ -20,13 +20,10 @@ import java.io.InputStream;
 import java.util.Map;
 
 import static com.fasterxml.jackson.core.JsonParser.Feature.STRICT_DUPLICATE_DETECTION;
-import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
 
 public class StateMachineInitializer {
     private static final ObjectMapper yamlOm =
-            new ObjectMapper(new YAMLFactory())
-                    .configure(STRICT_DUPLICATE_DETECTION, true)
-                    .configure(FAIL_ON_UNKNOWN_PROPERTIES, false);
+            new ObjectMapper(new YAMLFactory()).configure(STRICT_DUPLICATE_DETECTION, true);
     private static final ObjectMapper om = new ObjectMapper();
     private Map<String, State> journeyStates;
     private Map<String, NestedJourneyDefinition> nestedJourneyDefinitions;
