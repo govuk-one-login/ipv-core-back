f9017515a87ad384d5ce613a44f968966babe1e2
Shivanshu Patel <shivanshuit914@gmail.com>
2024-05-14 10:40:38 +0100

    PYIC-5776: skip device information in audit event when value is null

--- libs/audit-service/src/main/java/uk/gov/di/ipv/core/library/auditing/restricted/AuditRestrictedDeviceInformation.java
+++ libs/audit-service/src/main/java/uk/gov/di/ipv/core/library/auditing/restricted/AuditRestrictedDeviceInformation.java
@@ -1,5 +1,6 @@
 package uk.gov.di.ipv.core.library.auditing.restricted;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import uk.gov.di.ipv.core.library.annotations.ExcludeFromGeneratedCoverageReport;
@@ -7,11 +8,14 @@ import uk.gov.di.ipv.core.library.annotations.ExcludeFromGeneratedCoverageReport
 @ExcludeFromGeneratedCoverageReport
 @Getter
 public class AuditRestrictedDeviceInformation implements AuditRestricted {
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonProperty("device_information")
     private final DeviceInformation deviceInformation;
 
     public AuditRestrictedDeviceInformation(
-            @JsonProperty(value = "device_information", required = true) String deviceInformation) {
-        this.deviceInformation = new DeviceInformation(deviceInformation);
+            @JsonProperty(value = "device_information", required = false)
+                    String deviceInformation) {
+        this.deviceInformation =
+                deviceInformation != null ? new DeviceInformation(deviceInformation) : null;
     }
 }
--- libs/audit-service/src/main/java/uk/gov/di/ipv/core/library/auditing/restricted/DeviceInformation.java
+++ libs/audit-service/src/main/java/uk/gov/di/ipv/core/library/auditing/restricted/DeviceInformation.java
@@ -1,5 +1,6 @@
 package uk.gov.di.ipv.core.library.auditing.restricted;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import uk.gov.di.ipv.core.library.annotations.ExcludeFromGeneratedCoverageReport;
@@ -7,10 +8,11 @@ import uk.gov.di.ipv.core.library.annotations.ExcludeFromGeneratedCoverageReport
 @ExcludeFromGeneratedCoverageReport
 @Getter
 public class DeviceInformation {
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonProperty("encoded")
     private final String encoded;
 
     public DeviceInformation(@JsonProperty(value = "encoded", required = true) String encoded) {
-        this.encoded = encoded != null ? encoded : "";
+        this.encoded = encoded;
     }
 }
