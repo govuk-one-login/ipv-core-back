b0b23a9cbf37747dac33da0761386340feab8106
Thea Bautista <thea.bautista@digital.cabinet-office.gov.uk>
2024-06-11 09:40:52 +0100

    PYIC-6652: simplify ReverificationStatus enum

--- libs/common-services/src/main/java/uk/gov/di/ipv/core/library/domain/ReverificationStatus.java
+++ libs/common-services/src/main/java/uk/gov/di/ipv/core/library/domain/ReverificationStatus.java
@@ -6,12 +6,6 @@ import uk.gov.di.ipv.core.library.annotations.ExcludeFromGeneratedCoverageReport
 @Getter
 @ExcludeFromGeneratedCoverageReport
 public enum ReverificationStatus {
-    SUCCESS("success"),
-    FAILED("failed");
-
-    private final String status;
-
-    ReverificationStatus(String status) {
-        this.status = status;
-    }
+    SUCCESS,
+    FAILED;
 }
