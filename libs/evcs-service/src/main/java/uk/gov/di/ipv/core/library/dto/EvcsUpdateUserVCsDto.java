450773eecc6fc1f42715cf324b9615aa147ea46b
Rohit Payal <rohit.payal@digital.cabinet-office.gov.uk>
2024-06-13 13:15:09 +0100

    PYIC-6767 Updated evcs request dto to ignore null properties.

--- libs/evcs-service/src/main/java/uk/gov/di/ipv/core/library/dto/EvcsUpdateUserVCsDto.java
+++ libs/evcs-service/src/main/java/uk/gov/di/ipv/core/library/dto/EvcsUpdateUserVCsDto.java
@@ -1,5 +1,7 @@
 package uk.gov.di.ipv.core.library.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import uk.gov.di.ipv.core.library.enums.EvcsVCState;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record EvcsUpdateUserVCsDto(String signature, EvcsVCState state, Object metadata) {}
