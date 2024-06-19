eda381d7af780a946370df8715542f033c31084f
Sam Barker <samuel.barker@digital.cabinet-office.gov.uk>
2024-06-19 10:43:45 +0100

    use pending return from evcs to check identity and update it when starting a new journey

--- libs/evcs-service/src/main/java/uk/gov/di/ipv/core/library/service/EvcsService.java
+++ libs/evcs-service/src/main/java/uk/gov/di/ipv/core/library/service/EvcsService.java
@@ -19,7 +19,6 @@ import java.util.ArrayList;
 import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Stream;
 import static uk.gov.di.ipv.core.library.enums.EvcsVCState.CURRENT;
 import static uk.gov.di.ipv.core.library.enums.EvcsVCState.PENDING_RETURN;
 
