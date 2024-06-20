7402d11dfaeb64e5dec9e4c8b349a55e39cb7168
Sam Barker <samuel.barker@digital.cabinet-office.gov.uk>
2024-06-20 14:55:33 +0100

    spotless

--- libs/evcs-service/src/main/java/uk/gov/di/ipv/core/library/service/EvcsService.java
+++ libs/evcs-service/src/main/java/uk/gov/di/ipv/core/library/service/EvcsService.java
@@ -19,6 +19,7 @@ import java.util.ArrayList;
 import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
+
 import static uk.gov.di.ipv.core.library.enums.EvcsVCState.CURRENT;
 import static uk.gov.di.ipv.core.library.enums.EvcsVCState.PENDING_RETURN;
 
