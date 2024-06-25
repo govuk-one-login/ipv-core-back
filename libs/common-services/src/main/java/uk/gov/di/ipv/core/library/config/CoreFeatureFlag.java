94d2def77501ef50b3833811289e3107548b3619
Joe Edwards <joe.edwards@digital.cabinet-office.gov.uk>
2024-06-25 10:25:29 +0100

    PYIC-5759 Add test for prod logging

--- libs/common-services/src/main/java/uk/gov/di/ipv/core/library/config/CoreFeatureFlag.java
+++ libs/common-services/src/main/java/uk/gov/di/ipv/core/library/config/CoreFeatureFlag.java
@@ -9,8 +9,7 @@ public enum CoreFeatureFlag implements FeatureFlag {
     TICF_CRI_BETA("ticfCriBeta"),
     EVCS_WRITE_ENABLED("evcsWriteEnabled"),
     EVCS_READ_ENABLED("evcsReadEnabled"),
-    MFA_RESET("mfaResetEnabled"),
-    PARSE_VC_CLASSES("parseVcClasses");
+    MFA_RESET("mfaResetEnabled");
 
     private final String name;
 
