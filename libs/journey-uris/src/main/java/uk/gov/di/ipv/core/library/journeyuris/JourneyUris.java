f0d1caa43452e173e31ef6a2d49f193931bbd33f
AmritSidhu <amrit.sidhu@digital.cabinet-office.gov.uk>
2024-06-26 14:57:05 +0100

    PYIC-5097: add an event to handle the invalid_request oauth error

--- libs/journey-uris/src/main/java/uk/gov/di/ipv/core/library/journeyuris/JourneyUris.java
+++ libs/journey-uris/src/main/java/uk/gov/di/ipv/core/library/journeyuris/JourneyUris.java
@@ -34,4 +34,5 @@ public class JourneyUris {
             "/journey/temporarily-unavailable";
     public static final String JOURNEY_UNMET_PATH = "/journey/unmet";
     public static final String JOURNEY_VCS_NOT_CORRELATED = "/journey/vcs-not-correlated";
+    public static final String JOURNEY_INVALID_REQUEST_PATH = "/journey/invalid-request";
 }
