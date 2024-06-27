8539381e882213c6c3ea0b3146ba8ec14c829953
Joe Edwards <joe.edwards@digital.cabinet-office.gov.uk>
2024-06-27 09:52:00 +0100

    PYIC-5759 Update GPG45 profile evaluator to use vocab

--- libs/gpg45-evaluator/src/main/java/uk/gov/di/ipv/core/library/gpg45/Gpg45ProfileEvaluator.java
+++ libs/gpg45-evaluator/src/main/java/uk/gov/di/ipv/core/library/gpg45/Gpg45ProfileEvaluator.java
@@ -1,35 +1,27 @@
 package uk.gov.di.ipv.core.library.gpg45;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.type.CollectionType;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.message.StringMapMessage;
 import uk.gov.di.ipv.core.library.domain.VerifiableCredential;
 import uk.gov.di.ipv.core.library.exceptions.CredentialParseException;
-import uk.gov.di.ipv.core.library.gpg45.domain.CredentialEvidenceItem;
 import uk.gov.di.ipv.core.library.gpg45.enums.Gpg45Profile;
 import uk.gov.di.ipv.core.library.gpg45.exception.UnknownEvidenceTypeException;
+import uk.gov.di.model.CheckDetails;
+import uk.gov.di.model.IdentityCheck;
+import uk.gov.di.model.IdentityCheckCredential;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Stream;
 
-import static uk.gov.di.ipv.core.library.domain.VerifiableCredentialConstants.VC_CLAIM;
-import static uk.gov.di.ipv.core.library.domain.VerifiableCredentialConstants.VC_EVIDENCE;
+import static java.util.Objects.requireNonNullElse;
+import static software.amazon.awssdk.utils.CollectionUtils.isNullOrEmpty;
 import static uk.gov.di.ipv.core.library.helpers.LogHelper.LogField.LOG_GPG45_PROFILE;
 import static uk.gov.di.ipv.core.library.helpers.LogHelper.LogField.LOG_MESSAGE_DESCRIPTION;
 
 public class Gpg45ProfileEvaluator {
     private static final Logger LOGGER = LogManager.getLogger();
-    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
-    private static final CollectionType CREDENTIAL_EVIDENCE_ITEM_LIST_TYPE =
-            OBJECT_MAPPER
-                    .getTypeFactory()
-                    .constructCollectionType(List.class, CredentialEvidenceItem.class);
     private static final int NO_SCORE = 0;
 
     public Optional<Gpg45Profile> getFirstMatchingProfile(
@@ -56,150 +48,71 @@ public class Gpg45ProfileEvaluator {
 
     public Gpg45Scores buildScore(List<VerifiableCredential> vcs)
             throws UnknownEvidenceTypeException, CredentialParseException {
-        var evidenceMap = parseGpg45ScoresFromCredentials(vcs);
-        processEvidenceItems(evidenceMap, CredentialEvidenceItem.EvidenceType.DCMAW);
-        processEvidenceItems(evidenceMap, CredentialEvidenceItem.EvidenceType.F2F);
-        processFraudWithActivityItems(evidenceMap);
+
+        var identityChecks =
+                vcs.stream()
+                        .filter(vc -> vc.getCredential() instanceof IdentityCheckCredential)
+                        .flatMap(
+                                vc ->
+                                        ((IdentityCheckCredential) vc.getCredential())
+                                                .getEvidence().stream())
+                        .toList();
 
         return Gpg45Scores.builder()
-                .withActivity(
-                        extractMaxScoreFromEvidenceMap(
-                                evidenceMap, CredentialEvidenceItem.EvidenceType.ACTIVITY))
-                .withFraud(
-                        extractMaxScoreFromEvidenceMap(
-                                evidenceMap, CredentialEvidenceItem.EvidenceType.IDENTITY_FRAUD))
-                .withVerification(
-                        extractMaxScoreFromEvidenceMap(
-                                evidenceMap, CredentialEvidenceItem.EvidenceType.VERIFICATION))
-                .withEvidences(
-                        evidenceMap.get(CredentialEvidenceItem.EvidenceType.EVIDENCE).stream()
-                                .map(CredentialEvidenceItem::getEvidenceScore)
-                                .toList())
+                .withActivity(getMaxActivityScore(identityChecks))
+                .withFraud(getMaxFraudScore(identityChecks))
+                .withVerification(getMaxVerificationScore(identityChecks))
+                .withEvidences(getEvidences(identityChecks))
                 .build();
     }
 
-    private void processFraudWithActivityItems(
-            Map<CredentialEvidenceItem.EvidenceType, List<CredentialEvidenceItem>> evidenceMap) {
-        for (CredentialEvidenceItem evidenceItem :
-                evidenceMap.get(CredentialEvidenceItem.EvidenceType.FRAUD_WITH_ACTIVITY)) {
-            evidenceMap
-                    .get(CredentialEvidenceItem.EvidenceType.ACTIVITY)
-                    .add(
-                            new CredentialEvidenceItem(
-                                    CredentialEvidenceItem.EvidenceType.ACTIVITY,
-                                    evidenceItem.getActivityHistoryScore(),
-                                    Collections.emptyList()));
-            evidenceMap
-                    .get(CredentialEvidenceItem.EvidenceType.IDENTITY_FRAUD)
-                    .add(
-                            new CredentialEvidenceItem(
-                                    CredentialEvidenceItem.EvidenceType.IDENTITY_FRAUD,
-                                    evidenceItem.getIdentityFraudScore(),
-                                    Collections.emptyList()));
-        }
-    }
-
-    private void processEvidenceItems(
-            Map<CredentialEvidenceItem.EvidenceType, List<CredentialEvidenceItem>> evidenceMap,
-            CredentialEvidenceItem.EvidenceType evidenceType)
-            throws UnknownEvidenceTypeException {
-        List<CredentialEvidenceItem> evidenceItems = evidenceMap.get(evidenceType);
-        for (CredentialEvidenceItem evidenceItem : evidenceItems) {
-            List<CredentialEvidenceItem> gpg45EvidenceItems =
-                    convertEvidenceItemToGpg45EvidenceItems(evidenceItem);
-            for (CredentialEvidenceItem gpg45EvidenceItem : gpg45EvidenceItems) {
-                evidenceMap.get(gpg45EvidenceItem.getEvidenceType()).add(gpg45EvidenceItem);
-            }
-        }
-    }
-
-    private boolean isRelevantEvidence(CredentialEvidenceItem evidenceItem)
-            throws UnknownEvidenceTypeException {
-        return (evidenceItem.getEvidenceType().equals(CredentialEvidenceItem.EvidenceType.DCMAW)
-                || evidenceItem.getEvidenceType().equals(CredentialEvidenceItem.EvidenceType.F2F));
-    }
-
-    private List<CredentialEvidenceItem> convertEvidenceItemToGpg45EvidenceItems(
-            CredentialEvidenceItem evidenceItem) throws UnknownEvidenceTypeException {
-        if (!isRelevantEvidence(evidenceItem)) {
-            return Collections.emptyList();
-        }
-        List<CredentialEvidenceItem> gpg45CredentialItems = new ArrayList<>();
-
-        gpg45CredentialItems.add(
-                CredentialEvidenceItem.builder()
-                        .strengthScore(evidenceItem.getStrengthScore())
-                        .validityScore(evidenceItem.getValidityScore())
-                        .ci(evidenceItem.getCi())
-                        .build());
-
-        int verificationScore = evidenceItem.getVerificationScore();
-        if (evidenceItem.getEvidenceType().equals(CredentialEvidenceItem.EvidenceType.DCMAW)
-                && evidenceItem.getActivityHistoryScore() != null) {
-
-            gpg45CredentialItems.add(
-                    new CredentialEvidenceItem(
-                            CredentialEvidenceItem.EvidenceType.ACTIVITY,
-                            evidenceItem.getActivityHistoryScore(),
-                            Collections.emptyList()));
-        }
-
-        gpg45CredentialItems.add(
-                new CredentialEvidenceItem(
-                        CredentialEvidenceItem.EvidenceType.VERIFICATION,
-                        verificationScore,
-                        Collections.emptyList()));
-
-        return gpg45CredentialItems;
-    }
-
-    private List<CredentialEvidenceItem> getCredentialEvidence(VerifiableCredential vc)
-            throws CredentialParseException {
-        var evidenceArray =
-                OBJECT_MAPPER.valueToTree(vc.getClaimsSet().getClaim(VC_CLAIM)).path(VC_EVIDENCE);
-
-        if (evidenceArray.isMissingNode()) {
-            return Collections.emptyList();
-        }
-
-        try {
-            return OBJECT_MAPPER.treeToValue(evidenceArray, CREDENTIAL_EVIDENCE_ITEM_LIST_TYPE);
-        } catch (JsonProcessingException e) {
-            throw new CredentialParseException("Unable to create credential evidence list", e);
-        }
-    }
-
-    private Map<CredentialEvidenceItem.EvidenceType, List<CredentialEvidenceItem>>
-            parseGpg45ScoresFromCredentials(List<VerifiableCredential> vcs)
-                    throws UnknownEvidenceTypeException, CredentialParseException {
-        Map<CredentialEvidenceItem.EvidenceType, List<CredentialEvidenceItem>> evidenceMap =
-                Map.of(
-                        CredentialEvidenceItem.EvidenceType.ACTIVITY, new ArrayList<>(),
-                        CredentialEvidenceItem.EvidenceType.EVIDENCE, new ArrayList<>(),
-                        CredentialEvidenceItem.EvidenceType.IDENTITY_FRAUD, new ArrayList<>(),
-                        CredentialEvidenceItem.EvidenceType.VERIFICATION, new ArrayList<>(),
-                        CredentialEvidenceItem.EvidenceType.DCMAW, new ArrayList<>(),
-                        CredentialEvidenceItem.EvidenceType.F2F, new ArrayList<>(),
-                        CredentialEvidenceItem.EvidenceType.FRAUD_WITH_ACTIVITY, new ArrayList<>(),
-                        CredentialEvidenceItem.EvidenceType.NINO, new ArrayList<>());
-
-        for (var vc : vcs) {
-            var credentialEvidenceList = getCredentialEvidence(vc);
-            for (CredentialEvidenceItem evidenceItem : credentialEvidenceList) {
-                evidenceItem.setCredentialIss(vc.getClaimsSet().getIssuer());
-                evidenceMap.get(evidenceItem.getEvidenceType()).add(evidenceItem);
-            }
-        }
-
-        return evidenceMap;
-    }
-
-    private Integer extractMaxScoreFromEvidenceMap(
-            Map<CredentialEvidenceItem.EvidenceType, List<CredentialEvidenceItem>> evidenceMap,
-            CredentialEvidenceItem.EvidenceType evidenceType) {
-        return evidenceMap.get(evidenceType).stream()
-                .max(evidenceType.getComparator())
-                .map(evidenceType.getScoreGetter())
+    private int getMaxActivityScore(List<IdentityCheck> identityChecks) {
+        return identityChecks.stream()
+                .mapToInt(check -> requireNonNullElse(check.getActivityHistoryScore(), NO_SCORE))
+                .max()
                 .orElse(NO_SCORE);
+    }
+
+    private int getMaxFraudScore(List<IdentityCheck> identityChecks) {
+        return identityChecks.stream()
+                .mapToInt(check -> requireNonNullElse(check.getIdentityFraudScore(), NO_SCORE))
+                .max()
+                .orElse(NO_SCORE);
+    }
+
+    private int getMaxVerificationScore(List<IdentityCheck> identityChecks) {
+        return identityChecks.stream().mapToInt(this::getVerificationScore).max().orElse(NO_SCORE);
+    }
+
+    private int getVerificationScore(IdentityCheck identityCheck) {
+        if (identityCheck.getVerificationScore() != null) {
+            return identityCheck.getVerificationScore();
+        }
+        if (!isNullOrEmpty(identityCheck.getCheckDetails())) {
+            return identityCheck.getCheckDetails().stream()
+                    .filter(
+                            checkMethod ->
+                                    checkMethod.getBiometricVerificationProcessLevel() != null)
+                    .findFirst()
+                    .map(CheckDetails::getBiometricVerificationProcessLevel)
+                    .orElse(NO_SCORE);
+        }
+        return NO_SCORE;
+    }
+
+    private List<Gpg45Scores.Evidence> getEvidences(List<IdentityCheck> identityChecks) {
+        return identityChecks.stream()
+                .flatMap(
+                        identityCheck -> {
+                            if (identityCheck.getStrengthScore() != null
+                                    && identityCheck.getValidityScore() != null) {
+                                return Stream.of(
+                                        new Gpg45Scores.Evidence(
+                                                identityCheck.getStrengthScore(),
+                                                identityCheck.getValidityScore()));
+                            }
+                            return Stream.empty();
+                        })
+                .toList();
     }
 }
