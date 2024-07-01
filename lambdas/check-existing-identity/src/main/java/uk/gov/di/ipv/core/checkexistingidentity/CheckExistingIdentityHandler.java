c74b9c692432c6a29689f445d812b2cefc68e4ad
Sam Barker <samuel.barker@digital.cabinet-office.gov.uk>
2024-07-01 10:01:58 +0100

    combine checks

--- lambdas/check-existing-identity/src/main/java/uk/gov/di/ipv/core/checkexistingidentity/CheckExistingIdentityHandler.java
+++ lambdas/check-existing-identity/src/main/java/uk/gov/di/ipv/core/checkexistingidentity/CheckExistingIdentityHandler.java
@@ -388,26 +388,26 @@ public class CheckExistingIdentityHandler
         if (isF2f && evcsVcs.size() == 1) {
             return true;
         }
-        // Tactical contains an extra non-F2F VC
-        if (tacticalVcs.size() == evcsVcs.size() + 1) {
-            return true;
-        }
         // Tactical contains the same as pending EVCS, with one extra F2F VC
-        var extraF2fVcs =
-                tacticalVcs.stream()
-                        .filter(
-                                credential ->
-                                        F2F.getId().equals(credential.getCriId())
-                                                && evcsVcs.stream()
-                                                        .noneMatch(
-                                                                evcsVC ->
-                                                                        evcsVC.getVcString()
-                                                                                .equals(
-                                                                                        credential
-                                                                                                .getVcString())))
-                        .toList();
+        if (tacticalVcs.size() == evcsVcs.size() + 1) {
 
-        return extraF2fVcs.size() == 1;
+            var extraF2fVcs =
+                    tacticalVcs.stream()
+                            .filter(
+                                    credential ->
+                                            F2F.getId().equals(credential.getCriId())
+                                                    && evcsVcs.stream()
+                                                            .noneMatch(
+                                                                    evcsVC ->
+                                                                            evcsVC.getVcString()
+                                                                                    .equals(
+                                                                                            credential
+                                                                                                    .getVcString())))
+                            .toList();
+
+            return extraF2fVcs.size() == 1;
+        }
+        return false;
     }
 
     @ExcludeFromGeneratedCoverageReport
