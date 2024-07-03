4fccd50ae3b78ab960102dbfd767ee8ddb78fafe
Sam Barker <samuel.barker@digital.cabinet-office.gov.uk>
2024-07-03 13:49:05 +0100

    remove duplicate test

--- lambdas/check-existing-identity/src/main/java/uk/gov/di/ipv/core/checkexistingidentity/CheckExistingIdentityHandler.java
+++ lambdas/check-existing-identity/src/main/java/uk/gov/di/ipv/core/checkexistingidentity/CheckExistingIdentityHandler.java
@@ -382,9 +382,7 @@ public class CheckExistingIdentityHandler
                                 isPendingEvcs,
                                 vcBundle.isF2fIdentity());
 
-                if (hasPartiallyMigratedVcs
-                        && evcsIdentityVcs.size() == 1
-                        && F2F.equals(evcsIdentityVcs.get(0).getCri())) {
+                if (hasPartiallyMigratedVcs) {
                     // use tactical vcs but with evcs flags so that the store-identity lambda is
                     // called next and updates the evcs pending one
                     return new VerifiableCredentialBundle(tacticalVcs, true, true);
