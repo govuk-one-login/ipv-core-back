12539e5edeb9abe31410a3c2bc24cb58a103c41c
Joe Edwards <joe.edwards@digital.cabinet-office.gov.uk>
2024-07-05 15:58:36 +0100

    PYIC-6883 Try Async AWS CRT Client

--- libs/audit-service/src/main/java/uk/gov/di/ipv/core/library/service/AuditService.java
+++ libs/audit-service/src/main/java/uk/gov/di/ipv/core/library/service/AuditService.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import software.amazon.awssdk.http.crt.AwsCrtAsyncHttpClient;
 import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 import software.amazon.awssdk.services.sqs.SqsClient;
@@ -54,7 +55,10 @@ public class AuditService {
                         .region(EU_WEST_2)
                         .httpClientBuilder(UrlConnectionHttpClient.builder())
                         .build(),
-                SqsAsyncClient.builder().region(EU_WEST_2).build());
+                SqsAsyncClient.builder()
+                        .region(EU_WEST_2)
+                        .httpClientBuilder(AwsCrtAsyncHttpClient.builder())
+                        .build());
     }
 
     public void sendAuditEvent(AuditEvent auditEvent) throws SqsException {
