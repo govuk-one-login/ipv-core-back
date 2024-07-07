1709d6af55dc0200beea362781481ac1f76f55da
Thea Bautista <thea.bautista@digital.cabinet-office.gov.uk>
2024-07-07 18:14:16 +0100

    PYIC-5759: add private constructors to override public implicit ones for new helpers

--- lambdas/build-proven-user-identity-details/src/main/java/uk/gov/di/ipv/core/buildprovenuseridentitydetails/domain/NameAndDateOfBirth.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.di.ipv.core.buildprovenuseridentitydetails.domain;
-
-import uk.gov.di.ipv.core.library.annotations.ExcludeFromGeneratedCoverageReport;
-import uk.gov.di.model.NamePart;
-
-import java.util.List;
-
-@ExcludeFromGeneratedCoverageReport
-public record NameAndDateOfBirth(String name, List<NamePart> nameParts, String dateOfBirth) {}
--- libs/common-services/src/main/java/uk/gov/di/ipv/core/library/helpers/BirthDateHelper.java
+++ libs/common-services/src/main/java/uk/gov/di/ipv/core/library/helpers/BirthDateHelper.java
@@ -3,6 +3,8 @@ package uk.gov.di.ipv.core.library.helpers;
 import uk.gov.di.model.BirthDate;
 
 public class BirthDateHelper {
+    private BirthDateHelper() {}
+
     public static BirthDate createBirthDate(String value) {
         var birthDate = new BirthDate();
         birthDate.setValue(value);
