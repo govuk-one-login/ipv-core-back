e14211b30096fcd857defeb4d490ccb8bcd2ea9d
Rohit Payal <rohit.payal@digital.cabinet-office.gov.uk>
2024-07-10 13:44:30 +0100

    PYIC-6855 Resolve sonar issue.

--- libs/common-services/src/main/java/uk/gov/di/ipv/core/library/exceptions/BatchDeleteException.java
+++ libs/common-services/src/main/java/uk/gov/di/ipv/core/library/exceptions/BatchDeleteException.java
@@ -1,5 +1,8 @@
 package uk.gov.di.ipv.core.library.exceptions;
 
+import uk.gov.di.ipv.core.library.annotations.ExcludeFromGeneratedCoverageReport;
+
+@ExcludeFromGeneratedCoverageReport
 public class BatchDeleteException extends Exception {
     public BatchDeleteException(String message) {
         super(message);
