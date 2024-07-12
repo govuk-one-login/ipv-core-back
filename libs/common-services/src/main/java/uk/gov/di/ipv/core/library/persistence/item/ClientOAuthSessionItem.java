0068c45ef1b5e3c690b0dbaaff4ca2fb57c86885
Shivanshu Patel <shivanshuit914@gmail.com>
2024-07-12 10:28:12 +0100

    Revert "PYIC-6899: use dynamic vot for the f2f cri evidence request"

--- libs/common-services/src/main/java/uk/gov/di/ipv/core/library/persistence/item/ClientOAuthSessionItem.java
+++ libs/common-services/src/main/java/uk/gov/di/ipv/core/library/persistence/item/ClientOAuthSessionItem.java
@@ -7,14 +7,10 @@ import lombok.NoArgsConstructor;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
 import uk.gov.di.ipv.core.library.annotations.ExcludeFromGeneratedCoverageReport;
-import uk.gov.di.ipv.core.library.domain.ProfileType;
 import uk.gov.di.ipv.core.library.enums.Vot;
-import uk.gov.di.ipv.core.library.service.ConfigService;
 
 import java.util.Arrays;
 import java.util.List;
-
-import static uk.gov.di.ipv.core.library.config.CoreFeatureFlag.P1_JOURNEYS_ENABLED;
 
 @DynamoDbBean
 @ExcludeFromGeneratedCoverageReport
@@ -49,25 +45,5 @@ public class ClientOAuthSessionItem implements DynamodbItem {
         return Vot.SUPPORTED_VOTS_BY_DESCENDING_STRENGTH.stream()
                 .filter(vot -> vtr.contains(vot.name()))
                 .toList();
-    }
-
-    public Vot getLowestStrengthRequestedGpg45Vot(ConfigService configService) {
-        var requestedGpg45VotsByStrengthDescending =
-                getRequestedVotsByStrength().stream()
-                        .filter(vot -> vot.getProfileType() == ProfileType.GPG45)
-                        .toList();
-
-        var lowestStrengthRequestedGpg45Vot =
-                requestedGpg45VotsByStrengthDescending.get(
-                        requestedGpg45VotsByStrengthDescending.size() - 1);
-
-        if (lowestStrengthRequestedGpg45Vot == Vot.P1
-                && !configService.enabled(P1_JOURNEYS_ENABLED)) {
-            lowestStrengthRequestedGpg45Vot =
-                    requestedGpg45VotsByStrengthDescending.get(
-                            requestedGpg45VotsByStrengthDescending.size() - 2);
-        }
-
-        return lowestStrengthRequestedGpg45Vot;
     }
 }
