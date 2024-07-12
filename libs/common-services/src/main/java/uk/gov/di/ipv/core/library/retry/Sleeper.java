f79d51b68421a1e01653685c7d3604a04b2b584e
Sam Barker <samuel.barker@digital.cabinet-office.gov.uk>
2024-07-12 16:02:20 +0100

    use @ExcludeFromGeneratedCoverageReport for sleeper

--- libs/common-services/src/main/java/uk/gov/di/ipv/core/library/retry/Sleeper.java
+++ libs/common-services/src/main/java/uk/gov/di/ipv/core/library/retry/Sleeper.java
@@ -1,5 +1,8 @@
 package uk.gov.di.ipv.core.library.retry;
 
+import uk.gov.di.ipv.core.library.annotations.ExcludeFromGeneratedCoverageReport;
+
+@ExcludeFromGeneratedCoverageReport
 public class Sleeper {
     public void sleep(long millis) throws InterruptedException {
         Thread.sleep(millis);
