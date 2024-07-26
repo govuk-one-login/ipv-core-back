dd335d7c429644dede2e694c246143cf64f0ea7e
Thea Bautista <thea.bautista@digital.cabinet-office.gov.uk>
2024-07-26 10:06:31 +0100

    PYIC-6021: update cimit base url config name

--- libs/common-services/src/main/java/uk/gov/di/ipv/core/library/config/ConfigurationVariable.java
+++ libs/common-services/src/main/java/uk/gov/di/ipv/core/library/config/ConfigurationVariable.java
@@ -7,7 +7,7 @@ public enum ConfigurationVariable {
     CIMIT_COMPONENT_ID("cimit/componentId"),
     CIMIT_CONFIG("cimit/config"),
     CIMIT_SIGNING_KEY("cimit/signingKey"),
-    CIMIT_INTERNAL_API_BASE_URL("cimit/internalApiBaseUrl"),
+    CIMIT_INTERNAL_API_BASE_URL("cimit/apiBaseUrl"),
     CIMIT_INTERNAL_API_KEY("cimitInternalApi"),
     CI_CONFIG("self/ci-config"),
     CI_SCORING_THRESHOLD("self/ciScoringThresholdByVot/%s"),
