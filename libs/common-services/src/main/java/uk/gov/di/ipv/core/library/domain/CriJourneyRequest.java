fed9e9bee5f6566e20784f0b386b2b67b6b9b0ec
mike.collingwood <mike.collingwood@digital.cabinet-office.gov.uk>
2024-08-15 14:27:11 +0100

    PYIC-6914: Remove redundant explicit CriJourneyRequest constructor

--- libs/common-services/src/main/java/uk/gov/di/ipv/core/library/domain/CriJourneyRequest.java
+++ libs/common-services/src/main/java/uk/gov/di/ipv/core/library/domain/CriJourneyRequest.java
@@ -15,22 +15,4 @@ import uk.gov.di.ipv.core.library.annotations.ExcludeFromGeneratedCoverageReport
 @SuperBuilder
 public class CriJourneyRequest extends JourneyRequest {
     private String language;
-
-    public CriJourneyRequest(
-            String ipvSessionId,
-            String ipAddress,
-            String deviceInformation,
-            String clientOAuthSessionId,
-            String journey,
-            String featureSet,
-            String language) {
-        super(
-                ipvSessionId,
-                ipAddress,
-                deviceInformation,
-                clientOAuthSessionId,
-                journey,
-                featureSet);
-        this.language = language;
-    }
 }
