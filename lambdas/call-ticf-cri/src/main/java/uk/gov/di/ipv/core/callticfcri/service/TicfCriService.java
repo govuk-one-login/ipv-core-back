c0039e3a6db6e6386a23cefbdeb548834cb0cd16
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2024-08-19 10:22:03 +0100

    PYIC-7198: Make ticf request timeout configurable
    This updates the ticfCriService to allow the request timeout to be
    configurable. A request should never take longer than 2 seconds to be
    processed by the TICF CRI, so we can reduce our own timeout.
    
    The config value comes from the RestCriConfig for the TICF CRI. We may
    not want to explicitly set a timeout on other CRIs, so the value is
    optional, defaulting to 30 seconds. Currently the TICF CRI is the only
    CRI that uses the RestCriConfig.

--- lambdas/call-ticf-cri/src/main/java/uk/gov/di/ipv/core/callticfcri/service/TicfCriService.java
+++ lambdas/call-ticf-cri/src/main/java/uk/gov/di/ipv/core/callticfcri/service/TicfCriService.java
@@ -24,6 +24,8 @@ import java.io.IOException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.net.http.HttpTimeoutException;
+import java.time.Duration;
 import java.util.List;
 
 import static uk.gov.di.ipv.core.library.domain.Cri.TICF;
@@ -40,6 +42,7 @@ public class TicfCriService {
     private final VerifiableCredentialValidator jwtValidator;
     private final SessionCredentialsService sessionCredentialsService;
 
+    @ExcludeFromGeneratedCoverageReport
     public TicfCriService(ConfigService configService) {
         this.configService = configService;
         this.httpClient = HttpClient.newHttpClient();
@@ -47,7 +50,7 @@ public class TicfCriService {
         this.sessionCredentialsService = new SessionCredentialsService(configService);
     }
 
-    @ExcludeFromGeneratedCoverageReport
+    // Used by contract tests
     public TicfCriService(
             ConfigService configService,
             VerifiableCredentialValidator jwtValidator,
@@ -92,9 +95,10 @@ public class TicfCriService {
                                     .map(VerifiableCredential::getVcString)
                                     .toList());
 
-            HttpRequest.Builder httpRequestBuilder =
+            var httpRequestBuilder =
                     HttpRequest.newBuilder()
                             .uri(ticfCriConfig.getCredentialUrl())
+                            .timeout(Duration.ofSeconds(ticfCriConfig.getRequestTimeout()))
                             .POST(
                                     HttpRequest.BodyPublishers.ofString(
                                             OBJECT_MAPPER.writeValueAsString(ticfCriRequest)));
@@ -134,6 +138,10 @@ public class TicfCriService {
             if (e instanceof InterruptedException) {
                 // This should never happen running in Lambda as it's single threaded.
                 Thread.currentThread().interrupt();
+            }
+
+            if (e instanceof HttpTimeoutException) {
+                LOGGER.warn(LogHelper.buildLogMessage("Request to TICF CRI has timed out"));
             }
             // In the case of unavailability, the TICF CRI is deemed optional.
             LOGGER.error(
