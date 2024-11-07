a565d38a85195ad6295d224db7ef818473be54f8
Thea Bautista <thea.bautista@digital.cabinet-office.gov.uk>
2024-11-07 16:19:57 +0000

    PYIC-7641: clear dcmaw vc when user records their DL details are incorrect

--- libs/common-services/src/main/java/uk/gov/di/ipv/core/library/enums/SessionCredentialsResetType.java
+++ libs/common-services/src/main/java/uk/gov/di/ipv/core/library/enums/SessionCredentialsResetType.java
@@ -2,6 +2,7 @@ package uk.gov.di.ipv.core.library.enums;
 
 public enum SessionCredentialsResetType {
     ALL,
+    DCMAW,
     NAME_ONLY_CHANGE,
     ADDRESS_ONLY_CHANGE,
     PENDING_F2F_ALL,
--- libs/verifiable-credentials/src/main/java/uk/gov/di/ipv/core/library/verifiablecredential/service/SessionCredentialsService.java
+++ libs/verifiable-credentials/src/main/java/uk/gov/di/ipv/core/library/verifiablecredential/service/SessionCredentialsService.java
@@ -22,6 +22,7 @@ import java.util.List;
 
 import static uk.gov.di.ipv.core.library.config.ConfigurationVariable.SESSION_CREDENTIALS_TTL;
 import static uk.gov.di.ipv.core.library.domain.Cri.ADDRESS;
+import static uk.gov.di.ipv.core.library.domain.Cri.DCMAW;
 import static uk.gov.di.ipv.core.library.domain.Cri.EXPERIAN_FRAUD;
 
 public class SessionCredentialsService {
@@ -108,6 +109,9 @@ public class SessionCredentialsService {
                                         item ->
                                                 List.of(ADDRESS.getId(), EXPERIAN_FRAUD.getId())
                                                         .contains(item.getCriId()))
+                                .toList();
+                        case DCMAW -> sessionCredentialItems.stream()
+                                .filter(item -> DCMAW.getId().equals(item.getCriId()))
                                 .toList();
                         case NAME_ONLY_CHANGE -> sessionCredentialItems.stream()
                                 .filter(item -> !item.getCriId().equals(ADDRESS.getId()))
