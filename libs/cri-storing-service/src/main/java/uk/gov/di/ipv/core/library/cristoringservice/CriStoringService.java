600e41045977e8b74de9bbd6d128f87b01e42e2b
Shivanshu Patel <shivanshuit914@gmail.com>
2024-11-20 17:48:57 +0000

    PYIC-7590: add TCIF cri check to submit session VCs

--- libs/cri-storing-service/src/main/java/uk/gov/di/ipv/core/library/cristoringservice/CriStoringService.java
+++ libs/cri-storing-service/src/main/java/uk/gov/di/ipv/core/library/cristoringservice/CriStoringService.java
@@ -150,10 +150,12 @@ public class CriStoringService {
                 new AuditEventUser(
                         userId, ipvSessionItem.getIpvSessionId(), govukSigninJourneyId, ipAddress);
 
-        List<VerifiableCredential> mitigationVcList =
-                new ArrayList<>(
-                        sessionCredentialsService.getCredentials(
-                                ipvSessionItem.getIpvSessionId(), userId, true));
+        List<VerifiableCredential> mitigationVcList = new ArrayList<>();
+        if (!cri.equals(TICF)) {
+            mitigationVcList.addAll(
+                    sessionCredentialsService.getCredentials(
+                            ipvSessionItem.getIpvSessionId(), userId, true));
+        }
 
         var scopeClaims = clientOAuthSessionItem.getScopeClaims();
         for (var vc : vcs) {
