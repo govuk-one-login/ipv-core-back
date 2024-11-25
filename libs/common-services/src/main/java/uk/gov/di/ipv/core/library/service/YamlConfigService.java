f598392d4500f18de6c78e04fbf34a80407cada6
Thea Bautista <thea.bautista@digital.cabinet-office.gov.uk>
2024-11-25 17:20:16 +0000

    PYIC-7757: add separate config for api tests when running core-back locally

--- libs/common-services/src/main/java/uk/gov/di/ipv/core/library/service/YamlConfigService.java
+++ libs/common-services/src/main/java/uk/gov/di/ipv/core/library/service/YamlConfigService.java
@@ -2,7 +2,9 @@ package uk.gov.di.ipv.core.library.service;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import uk.gov.di.ipv.core.library.annotations.ExcludeFromGeneratedCoverageReport;
 import uk.gov.di.ipv.core.library.exceptions.ConfigParameterNotFoundException;
 
 import java.io.File;
@@ -17,8 +19,10 @@ import static com.fasterxml.jackson.core.JsonParser.Feature.STRICT_DUPLICATE_DET
 public class YamlConfigService extends ConfigService {
     private static final File PARAMETERS_FILE = new File("./core.local.params.yaml");
     private static final File SECRETS_FILE = new File("./core.local.secrets.yaml");
+    private static final File API_TEST_CONFIG = new File("./core.api-tests.params.yaml");
     private static final String PATH_SEPARATOR = "/";
     private static final String CORE = "core";
+    private static final String CLIENTS = "clients";
     private static final String FEATURE_SETS = "features";
     private final ThreadLocal<List<String>> featureSet = new ThreadLocal<>();
 
@@ -40,17 +44,46 @@ public class YamlConfigService extends ConfigService {
     private final Map<String, String> parameters = new HashMap<>();
     private final Map<String, String> secrets = new HashMap<>();
 
-    public YamlConfigService() {
-        this(PARAMETERS_FILE, SECRETS_FILE);
+    public YamlConfigService(boolean useApiTestConfig) {
+        if (useApiTestConfig) {
+            addParamsAndSecretsToConfig(PARAMETERS_FILE, SECRETS_FILE, API_TEST_CONFIG);
+        }
+        addParamsAndSecretsToConfig(PARAMETERS_FILE, SECRETS_FILE, null);
     }
 
-    public YamlConfigService(File parametersFile, File secretsFile) {
+    public YamlConfigService() {
+        addParamsAndSecretsToConfig(PARAMETERS_FILE, SECRETS_FILE, null);
+    }
+
+    @ExcludeFromGeneratedCoverageReport
+    public YamlConfigService(File parametersFile, File secretsFile, File apiTestConfig) {
+        addParamsAndSecretsToConfig(parametersFile, secretsFile, apiTestConfig);
+    }
+
+    private void addParamsAndSecretsToConfig(
+            File parametersFile, File secretsFile, File apiTestConfig) {
         try {
-            var paramsYaml = YAML_OBJECT_MAPPER.readTree(parametersFile).get(CORE);
+            var paramsYaml = getParamsWithCorrectClientConfig(parametersFile, apiTestConfig);
             var secretsYaml = YAML_OBJECT_MAPPER.readTree(secretsFile).get(CORE);
 
             addConfig(parameters, paramsYaml);
             addConfig(secrets, secretsYaml);
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Could not load parameter files", e);
+        }
+    }
+
+    private JsonNode getParamsWithCorrectClientConfig(File parametersFile, File apiClientConfigFile)
+            throws IOException {
+        try {
+            var params = YAML_OBJECT_MAPPER.readTree(parametersFile).get(CORE);
+
+            if (apiClientConfigFile != null) {
+                var apiClientConfig = YAML_OBJECT_MAPPER.readTree(apiClientConfigFile).get(CLIENTS);
+                ((ObjectNode) params).replace(CLIENTS, apiClientConfig);
+            }
+
+            return params;
         } catch (IOException e) {
             throw new IllegalArgumentException("Could not load parameter files", e);
         }
--- local-running/src/main/java/uk/gov/di/ipv/coreback/CoreBack.java
+++ local-running/src/main/java/uk/gov/di/ipv/coreback/CoreBack.java
@@ -19,6 +19,7 @@ public class CoreBack {
 
     public CoreBack() throws IOException, URISyntaxException {
         ConfigService.setLocal(true);
+        ConfigService.setApiTest(shouldUseApiTestConfig());
 
         var lambdaHandler = new LambdaHandler();
         var journeyEngineHandler = new JourneyEngineHandler();
@@ -52,13 +53,18 @@ public class CoreBack {
         app.start(getPort());
     }
 
+    private boolean shouldUseApiTestConfig() {
+        var useApiTestConfig = System.getenv("USE_API_TEST_CONFIG");
+        return useApiTestConfig != null && useApiTestConfig.equals("true");
+    }
+
     private int getPort() {
         var envPort = System.getenv("PORT");
         return envPort == null ? DEFAULT_PORT : Integer.parseInt(envPort);
     }
 
     private void startAsyncPoller() throws URISyntaxException {
-        var configService = new YamlConfigService();
+        var configService = new YamlConfigService(false);
         var asyncQueueUrl = configService.getParameter("local/asyncQueue/apiBaseUrl");
         var asyncQueueApiKey = configService.getSecret("local/asyncQueue/apiKey");
         var asyncQueueName = configService.getSecret("local/asyncQueue/queueName");
