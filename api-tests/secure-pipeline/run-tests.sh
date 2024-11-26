faa6a8807f4372de9d7148d4a6329edebbda52d2
louiseli1 <louise.li@digital.cabinet-office.gov.uk>
2024-11-26 13:31:12 +0000

    Dev

--- api-tests/secure-pipeline/run-tests.sh
+++ api-tests/secure-pipeline/run-tests.sh
@@ -10,19 +10,19 @@ echo "Running API tests against the build environment"
 CORE_BACK_INTERNAL_API_KEY=$(aws secretsmanager get-secret-value --secret-id CoreBackInternalTestingApiKey | jq -r .SecretString)
 export CORE_BACK_INTERNAL_API_KEY
 
-EVCS_STUB_API_KEY=$(aws secretsmanager get-secret-value --secret-id /build/core/evcs/apiKey | jq -r .SecretString)
+EVCS_STUB_API_KEY=$(aws secretsmanager get-secret-value --secret-id /"$ENVIRONMENT"/core/evcs/apiKey | jq -r .SecretString)
 export EVCS_STUB_API_KEY
 
 CRI_STUB_GEN_CRED_API_KEY=$(aws secretsmanager get-secret-value --secret-id CriStubGenCredApiKey | jq -r .SecretString)
 export CRI_STUB_GEN_CRED_API_KEY
 
-MANAGEMENT_TICF_API_KEY=$(aws secretsmanager get-secret-value --secret-id /build/core/credentialIssuers/ticf/connections/stub/apiKey | jq -r .SecretString)
+MANAGEMENT_TICF_API_KEY=$(aws secretsmanager get-secret-value --secret-id /"$ENVIRONMENT"/core/credentialIssuers/ticf/connections/stub/apiKey | jq -r .SecretString)
 export MANAGEMENT_TICF_API_KEY
 
-MANAGEMENT_CIMIT_STUB_API_KEY=$(aws ssm get-parameter --name /tests/core-back-build/cimit_api_key | jq -r .Parameter.Value)
+MANAGEMENT_CIMIT_STUB_API_KEY=$(aws ssm get-parameter --name /tests/core-back-"$ENVIRONMENT"/cimit_api_key | jq -r .Parameter.Value)
 export MANAGEMENT_CIMIT_STUB_API_KEY
 
-CIMIT_INTERNAL_API_KEY=$(aws secretsmanager get-secret-value --secret-id /build/core/cimitApi/apiKey | jq -r .SecretString)
+CIMIT_INTERNAL_API_KEY=$(aws secretsmanager get-secret-value --secret-id /"$ENVIRONMENT"/core/cimitApi/apiKey | jq -r .SecretString)
 export CIMIT_INTERNAL_API_KEY
 
 cd /api-tests
