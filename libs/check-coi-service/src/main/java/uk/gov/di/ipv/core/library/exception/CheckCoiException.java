4a2cbcadd786773390a77c99195870bc5c95e1fa
Thea Bautista <thea.bautista@digital.cabinet-office.gov.uk>
2024-12-02 16:27:26 +0000

    PYIC-7797: migrate check-coi lambda to a new service

---
+++ libs/check-coi-service/src/main/java/uk/gov/di/ipv/core/library/exception/CheckCoiException.java
@@ -0,0 +1,7 @@
+package uk.gov.di.ipv.core.library.exception;
+
+public class CheckCoiException extends Exception {
+    public CheckCoiException(String message) {
+        super(message);
+    }
+}
