e2cf3b24e98f35214bb54fe784fc57c24f293fbe
Thea Bautista <thea.bautista@digital.cabinet-office.gov.uk>
2024-12-03 10:51:27 +0000

    PYIC-7797: WIP add ticfCriService to lambda

---
+++ lambdas/process-candidate-identity/src/main/java/uk/gov/di/ipv/core/processcandidateidentity/exception/TicfCriServiceException.java
@@ -0,0 +1,11 @@
+package uk.gov.di.ipv.core.processcandidateidentity.exception;
+
+public class TicfCriServiceException extends Exception {
+    public TicfCriServiceException(String message) {
+        super(message);
+    }
+
+    public TicfCriServiceException(Throwable cause) {
+        super(cause);
+    }
+}
