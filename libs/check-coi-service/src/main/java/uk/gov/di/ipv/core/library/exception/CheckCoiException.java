229e2d2bfa914e61d49f2aa9424492c8fdf8ff8a
Thea Bautista <thea.bautista@digital.cabinet-office.gov.uk>
2024-12-04 18:34:18 +0000

    PYIC-7797: remove check-coi service (the one outside of the lambda)

--- libs/check-coi-service/src/main/java/uk/gov/di/ipv/core/library/exception/CheckCoiException.java
+++
@@ -1,7 +0,0 @@
-package uk.gov.di.ipv.core.library.exception;
-
-public class CheckCoiException extends Exception {
-    public CheckCoiException(String message) {
-        super(message);
-    }
-}
