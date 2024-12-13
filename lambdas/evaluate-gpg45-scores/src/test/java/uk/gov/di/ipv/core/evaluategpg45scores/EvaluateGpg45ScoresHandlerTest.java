02c36390beef6ac62f51f088a1c3eb84b58bec93
Dan Corder <dan.corder@digital.cabinet-office.gov.uk>
2024-12-13 11:29:29 +0000

    PYIC-7650: Fix tests

--- lambdas/evaluate-gpg45-scores/src/test/java/uk/gov/di/ipv/core/evaluategpg45scores/EvaluateGpg45ScoresHandlerTest.java
+++ lambdas/evaluate-gpg45-scores/src/test/java/uk/gov/di/ipv/core/evaluategpg45scores/EvaluateGpg45ScoresHandlerTest.java
@@ -325,7 +325,7 @@ class EvaluateGpg45ScoresHandlerTest {
     }
 
     @Test
-    void shouldAllowM1cForApplicableAuthoritativeSourceFailedFraudCheck() throws Exception {
+    void shouldAddM1cToProfilesWhenAllowed() throws Exception {
         // Arrange
         var vcs =
                 List.of(
@@ -337,6 +337,7 @@ class EvaluateGpg45ScoresHandlerTest {
         when(sessionCredentialsService.getCredentials(TEST_SESSION_ID, TEST_USER_ID))
                 .thenReturn(vcs);
         when(userIdentityService.areVcsCorrelated(any())).thenReturn(true);
+        when(userIdentityService.allowM1C(vcs)).thenReturn(true);
         when(gpg45ProfileEvaluator.getFirstMatchingProfile(any(), eq(P2_PROFILES_PLUS_M1C)))
                 .thenReturn(Optional.of(M1C));
         when(gpg45ProfileEvaluator.buildScore(any()))
@@ -353,7 +354,7 @@ class EvaluateGpg45ScoresHandlerTest {
     }
 
     @Test
-    void shouldNotAllowM1cForNonFailedFraudCheck() throws Exception {
+    void shouldNotAddM1cToProfilesWhenNotAllowed() throws Exception {
         // Arrange
         var vcs =
                 List.of(
@@ -365,6 +366,7 @@ class EvaluateGpg45ScoresHandlerTest {
         when(sessionCredentialsService.getCredentials(TEST_SESSION_ID, TEST_USER_ID))
                 .thenReturn(vcs);
         when(userIdentityService.areVcsCorrelated(any())).thenReturn(true);
+        when(userIdentityService.allowM1C(vcs)).thenReturn(false);
         when(gpg45ProfileEvaluator.getFirstMatchingProfile(any(), eq(P2_PROFILES)))
                 .thenReturn(Optional.empty());
         when(gpg45ProfileEvaluator.buildScore(any()))
