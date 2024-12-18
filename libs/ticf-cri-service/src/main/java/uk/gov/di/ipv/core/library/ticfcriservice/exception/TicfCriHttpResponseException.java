a8002c8f6bab08521a93cfc6b921b1974c3dc5f7
Thea Bautista <thea.bautista@digital.cabinet-office.gov.uk>
2024-12-18 14:47:13 +0000

    PYIC-7797: move ticf cri service into library.

--- libs/ticf-cri-service/src/main/java/uk/gov/di/ipv/core/library/ticfcriservice/exception/TicfCriHttpResponseException.java
+++ libs/ticf-cri-service/src/main/java/uk/gov/di/ipv/core/library/ticfcriservice/exception/TicfCriHttpResponseException.java
@@ -1,4 +1,4 @@
-package uk.gov.di.ipv.core.processcandidateidentity.exception;
+package uk.gov.di.ipv.core.library.ticfcriservice.exception;
 
 public class TicfCriHttpResponseException extends Exception {
     public TicfCriHttpResponseException(String message) {
