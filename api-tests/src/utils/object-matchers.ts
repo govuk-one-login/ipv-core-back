f80d27ba90a723c8e5e90fbdb110964dadcae165
Thea Bautista <thea.bautista@digital.cabinet-office.gov.uk>
2025-01-14 10:47:41 +0000

    PYIC-7926: check for types instead of exact values for age

--- api-tests/src/utils/object-matchers.ts
+++ api-tests/src/utils/object-matchers.ts
@@ -3,6 +3,13 @@ export interface ObjectPartialEqualityResult {
   errorMessage?: string;
 }
 
+enum SupportedTypeMatchers {
+  "string",
+  "number",
+}
+
+const TYPE_CHECK_REGEX = /^type\[(\w+)\]$/;
+
 // In node v23, there is a feature called partialDeepStrictEqual which does what we want
 // However, since we are on node v22, we create a partial matcher from scratch.
 // When we upgrade to node >=v23, we could replace this matcher with partialDeepStrictEqual instead.
@@ -39,15 +46,57 @@ export const comparePartialEqualityBetweenObjects = (
       continue;
     }
 
-    if (expectedValue !== actualObject[expectedKey]) {
+    const actualValue = actualObject[expectedKey];
+    if (
+      typeof expectedValue === "string" &&
+      TYPE_CHECK_REGEX.test(expectedValue)
+    ) {
+      const validationRes = validateValueType(
+        expectedValue,
+        actualValue,
+        objectKeyHistory,
+      );
+
+      if (!validationRes.isPartiallyEqual) {
+        return validationRes;
+      }
+
+      objectKeyHistory.pop();
+      continue;
+    }
+
+    if (expectedValue !== actualValue) {
       return {
         isPartiallyEqual: false,
-        errorMessage: `Expected "${expectedValue}" for field ${formatKeyHistory(objectKeyHistory)} but got "${actualObject[expectedKey]}"`,
+        errorMessage: `Expected "${expectedValue}" for field ${formatKeyHistory(objectKeyHistory)} but got "${actualValue}"`,
       };
     }
     objectKeyHistory.pop();
   }
   return { isPartiallyEqual: true };
+};
+
+const validateValueType = (
+  expectedType: string,
+  actualValue: any,
+  keyHistory: string[],
+): ObjectPartialEqualityResult => {
+  const typeMatch = expectedType.match(TYPE_CHECK_REGEX);
+  if (!typeMatch) {
+    throw new Error(`${expectedType} is unsupported.`);
+  }
+
+  const type = typeMatch[1].toLowerCase();
+  if (type in SupportedTypeMatchers) {
+    return typeof actualValue === type
+      ? { isPartiallyEqual: true }
+      : {
+          isPartiallyEqual: false,
+          errorMessage: `Expected ${formatKeyHistory(keyHistory)} to be type ${type} but it was type ${typeof actualValue}`,
+        };
+  } else {
+    throw new Error(`${expectedType} is unsupported.`);
+  }
 };
 
 const formatKeyHistory = (keyHistory: string[]): string => {
