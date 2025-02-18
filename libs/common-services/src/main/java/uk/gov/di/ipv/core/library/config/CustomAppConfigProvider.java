eb91cd379d35c19e24c989f79a34a8f3304a39e1
mike.collingwood <mike.collingwood@digital.cabinet-office.gov.uk>
2025-02-18 15:09:18 +0000

    PYIC-7872: Suppress CustomAppConfigProvider warnings, since the class is a temporary copy with one change

--- libs/common-services/src/main/java/uk/gov/di/ipv/core/library/config/CustomAppConfigProvider.java
+++ libs/common-services/src/main/java/uk/gov/di/ipv/core/library/config/CustomAppConfigProvider.java
@@ -24,6 +24,7 @@ import java.util.Map;
  * fixed to check for empty configuration string.
  */
 @ExcludeFromGeneratedCoverageReport
+@SuppressWarnings("all")
 public class CustomAppConfigProvider extends BaseProvider {
 
     private final AppConfigDataClient client;
