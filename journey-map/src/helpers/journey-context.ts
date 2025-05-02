bcc97b5b401b4b5270188dead5e1ac32ecd78e39
Joe Edwards <joe.edwards@digital.cabinet-office.gov.uk>
2025-05-02 14:54:06 +0100

    PYIC-8284 Add journey-context tests

--- journey-map/src/helpers/journey-context.ts
+++ journey-map/src/helpers/journey-context.ts
@@ -1,34 +1,14 @@
-import { JourneyEvent, JourneyState } from "../types.js";
-
-const addJourneyContextFromDefinition = (
-  definition: JourneyEvent,
-  journeyContexts: string[],
-): void => {
-  Object.values(definition.checkIfDisabled || {}).forEach((def) => {
-    addJourneyContextFromDefinition(def, journeyContexts);
-  });
-
-  Object.entries(definition.checkJourneyContext || {}).forEach(([ctx, def]) => {
-    if (!journeyContexts.includes(ctx)) {
-      journeyContexts.push(ctx);
-    }
-    addJourneyContextFromDefinition(def, journeyContexts);
-  });
-
-  Object.values(definition.checkFeatureFlag || {}).forEach((def) => {
-    addJourneyContextFromDefinition(def, journeyContexts);
-  });
-};
+import { JourneyState } from "../types.js";
+import { resolveAllTargets } from "./event-resolver.js";
 
 export const getJourneyContexts = (
   journeyStates: Record<string, JourneyState>,
 ): string[] => {
-  const checkedJourneyContexts: string[] = [];
-  Object.values(journeyStates).forEach((definition) => {
-    const events = definition.events || definition.exitEvents || {};
-    Object.values(events).forEach((def) => {
-      addJourneyContextFromDefinition(def, checkedJourneyContexts);
-    });
-  });
-  return checkedJourneyContexts;
+  return [
+    ...new Set(
+      resolveAllTargets(journeyStates).flatMap((target) =>
+        Object.keys(target.checkJourneyContext ?? {}),
+      ),
+    ),
+  ];
 };
