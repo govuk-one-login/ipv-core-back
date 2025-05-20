00eac55c35b9fe5651f31f96b6553227745af72f
Dan Corder <dan.corder@digital.cabinet-office.gov.uk>
2025-05-20 11:12:59 +0100

    PYIC-8246: Run spotless

--- libs/ais-service/src/main/java/uk/gov/di/ipv/core/library/ais/enums/AisAuditLevel.java
+++ libs/ais-service/src/main/java/uk/gov/di/ipv/core/library/ais/enums/AisAuditLevel.java
@@ -3,6 +3,8 @@ package uk.gov.di.ipv.core.library.ais.enums;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public enum AisAuditLevel {
-    @JsonProperty("standard") STANDARD,
-    @JsonProperty("enhanced") ENHANCED
+    @JsonProperty("standard")
+    STANDARD,
+    @JsonProperty("enhanced")
+    ENHANCED
 }
