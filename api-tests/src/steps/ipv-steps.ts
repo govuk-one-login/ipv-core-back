21bf9a6a72f12bbc34444ef769d3c17b849ea0ea
mike.collingwood <mike.collingwood@digital.cabinet-office.gov.uk>
2025-06-11 12:11:37 +0100

    PYIC-8402: Fix strategic-app-cross-browser-journey audit events

--- api-tests/src/steps/ipv-steps.ts
+++ api-tests/src/steps/ipv-steps.ts
@@ -514,11 +514,6 @@ Then(
       const expectedEvents = await getAuditEventsForJourneyType(journeyName);
       const actualEvents = await auditClient.getAuditEvents(this.userId);
 
-      if (journeyName === "strategic-app-cross-browser-journey") {
-        // Find events with no userId (only IPV_APP_MISSING_CONTEXT)
-        actualEvents.push(...(await auditClient.getAuditEvents(undefined)));
-      }
-
       const comparisonResult = compareAuditEvents(actualEvents, expectedEvents);
       assert.ok(
         comparisonResult.isPartiallyEqual,
