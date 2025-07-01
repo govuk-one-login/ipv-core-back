11dd29df2a81555dc7dbac09dc4c4f3abc9a64f1
Thea Bautista <thea.bautista@digital.cabinet-office.gov.uk>
2025-07-01 15:35:33 +0100

    PYIC-8466: remove unnecessary JsonIgnore + json property name

--- lambdas/process-async-cri-credential/src/main/java/uk/gov/di/ipv/core/processasynccricredential/domain/BaseAsyncCriResponse.java
+++ lambdas/process-async-cri-credential/src/main/java/uk/gov/di/ipv/core/processasynccricredential/domain/BaseAsyncCriResponse.java
@@ -1,7 +1,5 @@
 package uk.gov.di.ipv.core.processasynccricredential.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -10,11 +8,8 @@ import uk.gov.di.ipv.core.library.annotations.ExcludeFromGeneratedCoverageReport
 @ExcludeFromGeneratedCoverageReport
 @Getter
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
-@JsonIgnoreProperties(ignoreUnknown = true)
 public abstract class BaseAsyncCriResponse {
     private final String userId;
     private final String oauthState;
-
-    @JsonProperty("govuk_signin_journey_id")
     private final String journeyId;
 }
