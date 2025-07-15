1f0fd53baf6951190580065fd66fbcb858bf700e
mike.collingwood <mike.collingwood@digital.cabinet-office.gov.uk>
2025-07-15 09:15:44 +0100

    PYIC-8506: Make AccountInterventionException message more descriptive

--- libs/ais-service/src/main/java/uk/gov/di/ipv/core/library/ais/exception/AccountInterventionException.java
+++ libs/ais-service/src/main/java/uk/gov/di/ipv/core/library/ais/exception/AccountInterventionException.java
@@ -5,6 +5,7 @@ import uk.gov.di.ipv.core.library.annotations.ExcludeFromGeneratedCoverageReport
 @ExcludeFromGeneratedCoverageReport
 public class AccountInterventionException extends Exception {
     public AccountInterventionException() {
-        super("Account intervention.");
+        super(
+                "Account intervention. This is thrown when an intervention has been discovered in ProcessCandidateIdentity.");
     }
 }
