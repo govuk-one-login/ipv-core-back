a6831a5f6dc04466fba270e1e31dffae9344b7d5
thebauSoftwire <120715154+thebauSoftwire@users.noreply.github.com>
2025-07-21 09:54:04 +0100

    Revert "PYIC-4934: cleanup old kbv cri"

--- libs/common-services/src/main/java/uk/gov/di/ipv/core/library/domain/Cri.java
+++ libs/common-services/src/main/java/uk/gov/di/ipv/core/library/domain/Cri.java
@@ -17,6 +17,7 @@ public enum Cri {
     DRIVING_LICENCE("drivingLicence"),
     DWP_KBV("dwpKbv"),
     EXPERIAN_FRAUD("fraud"),
+    KBV("kbv"),
     EXPERIAN_KBV("experianKbv"),
     F2F("f2f"),
     HMRC_MIGRATION("hmrcMigration", true),
@@ -26,7 +27,7 @@ public enum Cri {
 
     private final String id;
     private final boolean isOperationalCri;
-    private static final Set<Cri> KBV_CRIS = Set.of(DWP_KBV, EXPERIAN_KBV);
+    private static final Set<Cri> KBV_CRIS = Set.of(DWP_KBV, KBV, EXPERIAN_KBV);
     private static final String EXPERIAN_KBV_REDIRECT_ID = "kbv";
 
     Cri(String id) {
