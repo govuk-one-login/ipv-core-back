509f94140a36ce987dae2532c827b86d7a40a2bb
Thea Bautista <thea.bautista@digital.cabinet-office.gov.uk>
2025-07-21 10:10:14 +0100

    PYIC-4934: remove old KBV from CRI enum and remove unused CRI_EXPERIAN_KBV state

--- libs/gpg45-evaluator/src/main/java/uk/gov/di/ipv/core/library/gpg45/validators/Gpg45IdentityCheckValidator.java
+++ libs/gpg45-evaluator/src/main/java/uk/gov/di/ipv/core/library/gpg45/validators/Gpg45IdentityCheckValidator.java
@@ -23,7 +23,7 @@ public class Gpg45IdentityCheckValidator {
         return switch (cri) {
             case BAV, DRIVING_LICENCE, PASSPORT -> isEvidenceSuccessful(identityCheck);
             case EXPERIAN_FRAUD -> isFraudCheckSuccessful(identityCheck);
-            case DWP_KBV, KBV, EXPERIAN_KBV -> isVerificationSuccessful(identityCheck);
+            case DWP_KBV, EXPERIAN_KBV -> isVerificationSuccessful(identityCheck);
             case DCMAW, DCMAW_ASYNC, F2F, HMRC_MIGRATION ->
                     isEvidenceSuccessful(identityCheck) && isVerificationSuccessful(identityCheck);
             case NINO -> isNinoSuccessful(identityCheck);
