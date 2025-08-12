a38441a71dfb541af6b611527c7e6e97422f95d3
mike.collingwood <mike.collingwood@digital.cabinet-office.gov.uk>
2025-08-12 13:31:02 +0100

    PYIC-8547: Add update to API test step to set currentPage from page action

--- api-tests/src/steps/ipv-steps.ts
+++ api-tests/src/steps/ipv-steps.ts
@@ -17,11 +17,11 @@ import {
 } from "../utils/request-body-generators.js";
 import { getRandomString } from "../utils/random-string-generator.js";
 import {
-  isClientResponse,
-  isCriResponse,
-  isJourneyResponse,
-  isPageResponse,
-  JourneyEngineResponse,
+    isClientResponse,
+    isCriResponse,
+    isJourneyResponse,
+    isPageResponse,
+    JourneyEngineResponse, PageResponse,
 } from "../types/internal-api.js";
 import { getProvenIdentityDetails } from "../clients/core-back-internal-client.js";
 import {
@@ -282,6 +282,8 @@ When(
       this.ipvSessionId,
       this.featureSet,
       this.clientOAuthSessionId,
+      // Assumes currently on a page state
+      (this.lastJourneyEngineResponse as PageResponse).page
     );
   },
 );
