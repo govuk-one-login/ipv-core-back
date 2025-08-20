a42e08dcdf467e88f17fa3428aeed88b6d593f98
Dominik Tworek <dominik.tworek@digital.cabinet-office.gov.uk>
2025-08-20 14:37:27 +0100

    PYIC-8573: Add AccountInterventionEvaluator tests.

---
+++ libs/ais-service/src/test/java/uk/gov/di/ipv/core/library/ais/helper/AccountInterventionEvaluatorTest.java
@@ -0,0 +1,54 @@
+package uk.gov.di.ipv.core.library.ais.helper;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import uk.gov.di.ipv.core.library.dto.AccountInterventionState;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AccountInterventionEvaluatorTest {
+
+    @ParameterizedTest
+    @ArgumentsSource(InvalidAccountInterventionArgumentsProvider.class)
+    void shouldReturnTrueWhenProvideInvalidAccountIntervention(
+            AccountInterventionState accountInterventionState) {
+        assertTrue(AccountInterventionEvaluator.shouldInvalidateSession(accountInterventionState));
+    }
+
+    @ParameterizedTest
+    @MethodSource("getValidAccountInterventionState")
+    void shouldReturnFalseWhenProvideValidAccountIntervention(
+            AccountInterventionState accountInterventionState) {
+        assertFalse(AccountInterventionEvaluator.shouldInvalidateSession(accountInterventionState));
+    }
+
+    private static Stream<Arguments> getValidAccountInterventionState() {
+        return Stream.of(
+                Arguments.of(
+                        AccountInterventionState.builder()
+                                .isBlocked(false)
+                                .isSuspended(true)
+                                .isReproveIdentity(true)
+                                .isResetPassword(false)
+                                .build()),
+                Arguments.of(
+                        AccountInterventionState.builder()
+                                .isBlocked(false)
+                                .isSuspended(false)
+                                .isReproveIdentity(true)
+                                .isResetPassword(false)
+                                .build()),
+                Arguments.of(
+                        AccountInterventionState.builder()
+                                .isBlocked(false)
+                                .isSuspended(false)
+                                .isReproveIdentity(false)
+                                .isResetPassword(false)
+                                .build()));
+    }
+}
