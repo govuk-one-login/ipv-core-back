447c7fe5273f07c18d179b799ac3174a07826c41
Dominik Tworek <dominik.tworek@digital.cabinet-office.gov.uk>
2025-08-20 15:26:55 +0100

    PYIC-8573: Revert creation of factory method 'from'

--- libs/common-services/src/main/java/uk/gov/di/ipv/core/library/dto/AccountInterventionState.java
+++ libs/common-services/src/main/java/uk/gov/di/ipv/core/library/dto/AccountInterventionState.java
@@ -18,12 +18,4 @@ public class AccountInterventionState {
     private boolean isSuspended;
     private boolean isReproveIdentity;
     private boolean isResetPassword;
-
-    public static AccountInterventionState from(AccountInterventionState accountInterventionState) {
-        return new AccountInterventionState(
-                accountInterventionState.isBlocked(),
-                accountInterventionState.isSuspended(),
-                accountInterventionState.isReproveIdentity(),
-                accountInterventionState.isResetPassword());
-    }
 }
