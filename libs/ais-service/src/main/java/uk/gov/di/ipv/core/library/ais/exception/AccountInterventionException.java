b14b9a1989273523b62a9a00e23b685db208813b
Dominik Tworek <dominik.tworek@digital.cabinet-office.gov.uk>
2025-08-22 09:26:16 +0100

    PYIC-8573: Update AccountInterventionException message

--- libs/ais-service/src/main/java/uk/gov/di/ipv/core/library/ais/exception/AccountInterventionException.java
+++ libs/ais-service/src/main/java/uk/gov/di/ipv/core/library/ais/exception/AccountInterventionException.java
@@ -6,7 +6,6 @@ import uk.gov.di.ipv.core.library.annotations.ExcludeFromGeneratedCoverageReport
 public class AccountInterventionException extends Exception {
 
     public AccountInterventionException() {
-        super(
-                "Account intervention. This is thrown when an intervention has been discovered in ProcessCandidateIdentity.");
+        super("Not allowed account intervention has been detected.");
     }
 }
