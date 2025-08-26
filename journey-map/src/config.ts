313c93197c58bc6e30ccf8a12c6f6060f618f89e
mike.collingwood <mike.collingwood@digital.cabinet-office.gov.uk>
2025-08-26 15:38:45 +0100

    PYIC-8235: Enable CORS for analytics gateway and update journey map config

--- journey-map/src/config.ts
+++ journey-map/src/config.ts
@@ -5,6 +5,6 @@ dotenv.config();
 export default {
   journeyTransitionsEndpoint:
     process.env.JOURNEY_TRANSITIONS_ENDPOINT ??
-    "https://api-dev.01.dev.identity.account.gov.uk/journey-transitions", // Shared dev
+    "analytics-api-dev.01.dev.identity.account.gov.uk/journey-transitions", // Shared dev
   analyticsApiKey: process.env.ANALYTICS_API_KEY ?? "",
 };
