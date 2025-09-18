4ad077f9138f8e266aa1729fafa0ab983d9f6a71
Dominik Tworek <dominik.tworek@digital.cabinet-office.gov.uk>
2025-09-18 21:00:54 +0100

    PYIC-8593: Remove public modifier from test class.

--- libs/common-services/src/test/java/uk/gov/di/ipv/core/library/helpers/NumberHelperTest.java
+++ libs/common-services/src/test/java/uk/gov/di/ipv/core/library/helpers/NumberHelperTest.java
@@ -10,7 +10,7 @@ import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class NumberHelperTest {
+class NumberHelperTest {
 
     private static final int DEFAULT = 999;
 
