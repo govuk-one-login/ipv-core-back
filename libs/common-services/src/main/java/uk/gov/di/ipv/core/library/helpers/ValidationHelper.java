04d92bb9600e3dc4df294af3705c14689de9104e
Dominik Tworek <dominik.tworek@digital.cabinet-office.gov.uk>
2025-09-30 12:18:15 +0100

    Remove validation for govukSigninJourneyId. Keep max char - 256

--- libs/common-services/src/main/java/uk/gov/di/ipv/core/library/helpers/ValidationHelper.java
+++ libs/common-services/src/main/java/uk/gov/di/ipv/core/library/helpers/ValidationHelper.java
@@ -5,9 +5,6 @@ import java.util.regex.Pattern;
 
 public class ValidationHelper {
     private static final Pattern IPV_JOURNEY_ID_PATTERN = Pattern.compile("^[A-Za-z0-9_-]{43}$");
-    private static final Pattern GOVUK_SIGNIN_JOURNEY_ID =
-            Pattern.compile(
-                    "^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$");
 
     private ValidationHelper() {
         // prevent initialisation
@@ -18,12 +15,5 @@ public class ValidationHelper {
             return false;
         }
         return IPV_JOURNEY_ID_PATTERN.matcher(id).matches();
-    }
-
-    public static boolean isValidGovukSigninJourneyId(String id) {
-        if (Objects.isNull(id)) {
-            return false;
-        }
-        return GOVUK_SIGNIN_JOURNEY_ID.matcher(id).matches();
     }
 }
