788ca378da699c29529020c0eba2dd57be86ac56
Dan Corder <dan.corder@digital.cabinet-office.gov.uk>
2025-11-04 16:22:21 +0000

    PYIC-8694: Make error code getter explicit

--- lambdas/fetch-journey-transitions/src/main/java/uk/gov/di/ipv/core/fetchjourneytransitions/exceptions/FetchJourneyTransitionException.java
+++ lambdas/fetch-journey-transitions/src/main/java/uk/gov/di/ipv/core/fetchjourneytransitions/exceptions/FetchJourneyTransitionException.java
@@ -1,14 +1,16 @@
 package uk.gov.di.ipv.core.fetchjourneytransitions.exceptions;
 
-import lombok.Getter;
 import uk.gov.di.ipv.core.fetchjourneytransitions.domain.ErrorCode;
 
-@Getter
 public class FetchJourneyTransitionException extends Exception {
     private final ErrorCode errorCode;
 
     public FetchJourneyTransitionException(String errorMessage, ErrorCode rootCause) {
         super(errorMessage);
         this.errorCode = rootCause;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
     }
 }
