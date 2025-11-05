c3780b483f60d2fdc1caea1367bad49f8e4d5263
Dan Corder <dan.corder@digital.cabinet-office.gov.uk>
2025-11-05 13:53:54 +0000

    PYIC-8694: Remove redundant public modifier

--- lambdas/fetch-journey-transitions/src/test/java/uk/gov/di/ipv/core/fetchjourneytransitions/domain/ErrorResponseBodyTest.java
+++ lambdas/fetch-journey-transitions/src/test/java/uk/gov/di/ipv/core/fetchjourneytransitions/domain/ErrorResponseBodyTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class ErrorResponseBodyTest {
+class ErrorResponseBodyTest {
     @Test
     void shouldSerialiseAllFields() {
         // Arrange
