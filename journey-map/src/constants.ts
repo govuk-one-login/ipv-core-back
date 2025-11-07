c4a3ba7f0a6e98e585b68ae64b99d7186d008923
Dominik Tworek <dominik.tworek@digital.cabinet-office.gov.uk>
2025-11-07 14:47:53 +0000

    PYIC-8697: Move node description to the left, change labels to be more explicit.

--- journey-map/src/constants.ts
+++ journey-map/src/constants.ts
@@ -1,7 +1,7 @@
 export const JOURNEY_TYPES: Record<string, string> = {
   INITIAL_JOURNEY_SELECTION: "Initial journey selection",
-  NEW_P1_IDENTITY: "New P1 identity",
-  NEW_P2_IDENTITY: "New P2/P3 identity",
+  NEW_P1_IDENTITY: "New at least P1 identity",
+  NEW_P2_IDENTITY: "New at least P2 identity",
   REUSE_EXISTING_IDENTITY: "Reuse existing identity",
   UPDATE_NAME: "Update name",
   UPDATE_ADDRESS: "Update address",
