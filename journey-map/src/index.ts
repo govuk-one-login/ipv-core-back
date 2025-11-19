d6618ea8ff7dca0c8e7a3d71ece5169c9e3b528e
Dan Corder <dan.corder@digital.cabinet-office.gov.uk>
2025-11-19 17:45:53 +0000

    PYIC-8695: Separate out enrich transition logic and add tests

--- journey-map/src/index.ts
+++ journey-map/src/index.ts
@@ -20,6 +20,7 @@ import {
   mapStringToEnvironment,
 } from "./service/analyticsService.js";
 import { parseTransitionsApiForm } from "./helpers/analytics.js";
+import { enrichJourneyTransitionData } from "./helpers/enrich-transitions.js";
 
 type ClickHandler = (e: MouseEvent) => void;
 
@@ -618,48 +619,6 @@ const setupJourneyTransitionInput = (): void => {
     await updateView();
   });
 };
-
-function enrichJourneyTransitionData(
-  journeyTransitions: JourneyTransition[],
-  journeyMaps: Record<string, JourneyMap>,
-  nestedJourneyMaps: Record<string, NestedJourneyMap>,
-): JourneyTransition[] {
-  journeyTransitions.map((t) =>
-    addNestedJourneyEntryEvent(t, journeyMaps, nestedJourneyMaps),
-  );
-  return journeyTransitions;
-}
-
-function addNestedJourneyEntryEvent(
-  transition: JourneyTransition,
-  journeyMaps: Record<string, JourneyMap>,
-  nestedJourneyMaps: Record<string, NestedJourneyMap>,
-) {
-  let entryEvent;
-
-  if (transition.from.includes("/")) {
-    // This is a transition from a nested journey into another nested journey. The from field will contain something like:
-    // HIGHEST_LEVEL_NESTED_JOURNEY/MID_LEVEL_NESTED_JOURNEY/LOWEST_LEVEL_NESTED_JOURNEY/STATE_IN_LOWEST_LEVEL
-    const fromParts = transition.from.split("/");
-    const fromState = fromParts[fromParts.length - 1];
-    const lowestNestedJourney = fromParts[fromParts.length - 2];
-    const fromJourneyMap = nestedJourneyMaps[lowestNestedJourney];
-
-    entryEvent =
-      fromJourneyMap?.nestedJourneyStates?.[fromState]?.events?.[
-        transition.event
-      ]?.targetEntryEvent;
-  } else {
-    entryEvent =
-      journeyMaps[transition.fromJourney]?.states?.[transition.from]?.events?.[
-        transition.event
-      ]?.targetEntryEvent;
-  }
-
-  if (entryEvent) {
-    transition.toEntryEvent = entryEvent;
-  }
-}
 
 const initialize = async (): Promise<void> => {
   setupJourneyTransitionInput();
