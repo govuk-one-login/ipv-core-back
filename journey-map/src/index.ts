3bc05d55e748485c7926bd646467a2f95e2d2114
Dan Corder <dan.corder@digital.cabinet-office.gov.uk>
2025-11-19 15:44:07 +0000

    PYIC-8695: Add correct counts for nested journeys within other nested journeys

--- journey-map/src/index.ts
+++ journey-map/src/index.ts
@@ -609,7 +609,11 @@ const setupJourneyTransitionInput = (): void => {
     ).finally(() => {
       transitionsSubmitButton.disabled = false;
     });
-    enrichJourneyTransitionData(journeyTransitions, journeyMaps);
+    enrichJourneyTransitionData(
+      journeyTransitions,
+      journeyMaps,
+      nestedJourneys,
+    );
     setJourneyTransitionsData(journeyTransitions);
     await updateView();
   });
@@ -618,19 +622,39 @@ const setupJourneyTransitionInput = (): void => {
 function enrichJourneyTransitionData(
   journeyTransitions: JourneyTransition[],
   journeyMaps: Record<string, JourneyMap>,
+  nestedJourneyMaps: Record<string, NestedJourneyMap>,
 ): JourneyTransition[] {
-  journeyTransitions.map((t) => addNestedJourneyEntryEvent(t, journeyMaps));
+  journeyTransitions.map((t) =>
+    addNestedJourneyEntryEvent(t, journeyMaps, nestedJourneyMaps),
+  );
   return journeyTransitions;
 }
 
 function addNestedJourneyEntryEvent(
   transition: JourneyTransition,
   journeyMaps: Record<string, JourneyMap>,
+  nestedJourneyMaps: Record<string, NestedJourneyMap>,
 ) {
-  const entryEvent =
-    journeyMaps[transition.fromJourney]?.states?.[transition.from]?.events?.[
-      transition.event
-    ]?.targetEntryEvent;
+  let entryEvent;
+
+  if (transition.from.includes("/")) {
+    // This is a transition from a nested journey into another nested journey. The from field will contain something like:
+    // HIGHEST_LEVEL_NESTED_JOURNEY/MID_LEVEL_NESTED_JOURNEY/LOWEST_LEVEL_NESTED_JOURNEY/STATE_IN_LOWEST_LEVEL
+    const fromParts = transition.from.split("/");
+    const fromState = fromParts[fromParts.length - 1];
+    const lowestNestedJourney = fromParts[fromParts.length - 2];
+    const fromJourneyMap = nestedJourneyMaps[lowestNestedJourney];
+
+    entryEvent =
+      fromJourneyMap?.nestedJourneyStates?.[fromState]?.events?.[
+        transition.event
+      ]?.targetEntryEvent;
+  } else {
+    entryEvent =
+      journeyMaps[transition.fromJourney]?.states?.[transition.from]?.events?.[
+        transition.event
+      ]?.targetEntryEvent;
+  }
 
   if (entryEvent) {
     transition.toEntryEvent = entryEvent;
