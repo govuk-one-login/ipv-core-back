95e27084c4ccf7f67106e00dfac51e91ce2a939f
Dominik Tworek <dominik.tworek@digital.cabinet-office.gov.uk>
2026-06-23 16:03:43 +0100

    PYIC-9170: Extract page contexts strings to fields.
    Sonar check complained about repeated "reason" string 4 times in one file.

--- lambdas/process-journey-event/src/main/java/uk/gov/di/ipv/core/processjourneyevent/statemachine/validators/PageContextValidator.java
+++ lambdas/process-journey-event/src/main/java/uk/gov/di/ipv/core/processjourneyevent/statemachine/validators/PageContextValidator.java
@@ -4,32 +4,34 @@ import java.util.Map;
 import java.util.Set;
 
 public class PageContextValidator extends AbstractPageContextValidator {
+
     private static final Map<String, Set<String>> ALLOWED_CONTEXTS_BY_PAGE =
             Map.ofEntries(
-                    Map.entry("delete-handover", Set.of("journeyType")),
+                    Map.entry("delete-handover", Set.of(JOURNEY_TYPE)),
+                    Map.entry("need-more-information-confirm-change-details", Set.of(JOURNEY_TYPE)),
+                    Map.entry("no-photo-id-web-find-another-way", Set.of(REASON)),
+                    Map.entry("page-dcmaw-success", Set.of(NO_ADDRESS)),
+                    Map.entry("page-ipv-success", Set.of(JOURNEY_TYPE)),
+                    Map.entry("page-multiple-doc-check", Set.of(ALLOW_NINO)),
+                    Map.entry("page-update-name", Set.of(JOURNEY_TYPE)),
+                    Map.entry("photo-id-web-find-another-way", Set.of(REASON)),
+                    Map.entry("prove-identity-another-type-photo-id", Set.of(INVALID_DOC)),
+                    Map.entry("prove-identity-another-way", Set.of(REMOVE_F2F)),
+                    Map.entry("prove-identity-no-other-photo-id", Set.of(INVALID_DOC)),
+                    Map.entry("prove-identity-no-photo-id", Set.of(NINO_ONLY)),
+                    Map.entry("pyi-no-match", Set.of(REASON)),
+                    Map.entry("pyi-technical", Set.of(IS_UNRECOVERABLE)),
+                    Map.entry("pyi-triage-desktop-download-app", Set.of(SMARTPHONE, IS_APP_ONLY)),
+                    Map.entry("pyi-triage-mobile-download-app", Set.of(SMARTPHONE, IS_APP_ONLY)),
+                    Map.entry("pyi-triage-select-smartphone", Set.of(DEVICE_TYPE)),
                     Map.entry(
-                            "need-more-information-confirm-change-details", Set.of("journeyType")),
-                    Map.entry("no-photo-id-web-find-another-way", Set.of("reason")),
-                    Map.entry("page-dcmaw-success", Set.of("noAddress")),
-                    Map.entry("page-ipv-success", Set.of("journeyType")),
-                    Map.entry("page-multiple-doc-check", Set.of("allowNino")),
-                    Map.entry("page-update-name", Set.of("journeyType")),
-                    Map.entry("photo-id-web-find-another-way", Set.of("reason")),
-                    Map.entry("prove-identity-another-type-photo-id", Set.of("invalidDoc")),
-                    Map.entry("prove-identity-another-way", Set.of("removeF2f")),
-                    Map.entry("prove-identity-no-other-photo-id", Set.of("invalidDoc")),
-                    Map.entry("prove-identity-no-photo-id", Set.of("ninoOnly")),
-                    Map.entry("pyi-no-match", Set.of("reason")),
-                    Map.entry("pyi-technical", Set.of("isUnrecoverable")),
-                    Map.entry("pyi-triage-desktop-download-app", Set.of("smartphone", "isAppOnly")),
-                    Map.entry("pyi-triage-mobile-download-app", Set.of("smartphone", "isAppOnly")),
-                    Map.entry("pyi-triage-select-smartphone", Set.of("deviceType")),
-                    Map.entry("sorry-could-not-confirm-details", Set.of("isExistingIdentityValid")),
-                    Map.entry("sorry-technical-problem", Set.of("reason")),
+                            "sorry-could-not-confirm-details", Set.of(IS_EXISTING_IDENTITY_VALID)),
+                    Map.entry("sorry-technical-problem", Set.of(REASON)),
                     Map.entry(
-                            "uk-driving-licence-details-not-correct", Set.of("isFromStrategicApp")),
-                    Map.entry("update-details-failed", Set.of("isExistingIdentityInvalid")),
-                    Map.entry("update-name-date-birth", Set.of("journeyType")));
+                            "uk-driving-licence-details-not-correct",
+                            Set.of(IS_FROM_STRATEGIC_APP)),
+                    Map.entry("update-details-failed", Set.of(IS_EXISTING_IDENTITY_INVALID)),
+                    Map.entry("update-name-date-birth", Set.of(JOURNEY_TYPE)));
 
     @Override
     Map<String, Set<String>> getAllowedContextsByPage() {
--- lambdas/process-journey-event/src/test/java/uk/gov/di/ipv/core/processjourneyevent/statemachine/validators/TestPageContextValidator.java
+++ lambdas/process-journey-event/src/test/java/uk/gov/di/ipv/core/processjourneyevent/statemachine/validators/TestPageContextValidator.java
@@ -6,8 +6,8 @@ import java.util.Set;
 public class TestPageContextValidator extends AbstractPageContextValidator {
     private static final Map<String, Set<String>> ALLOWED_CONTEXTS_BY_PAGE =
             Map.ofEntries(
-                    Map.entry("page-id-for-page-state", Set.of("reason")),
-                    Map.entry("page-with-missing-context", Set.of("reason", "journeyType")));
+                    Map.entry("page-id-for-page-state", Set.of(REASON)),
+                    Map.entry("page-with-missing-context", Set.of(REASON, JOURNEY_TYPE)));
 
     @Override
     Map<String, Set<String>> getAllowedContextsByPage() {
