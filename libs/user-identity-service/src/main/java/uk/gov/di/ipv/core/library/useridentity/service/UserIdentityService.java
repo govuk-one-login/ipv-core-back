f01041b46794d7936c2ac09b53e5bf5e21d870a9
Amrit Sidhu <amrit.sidhu@digital.cabinet-office.gov.uk>
2026-09-21 12:58:13 +0100

    PYIC-9227: replace old correlation tests with getVcCorrelationResult suite
    Removed unit tests for individual name and DOB correlation methods and replaced
    them with test coverage for UserIdentityService.getVcCorrelationResult.
    
    remove separate correlation methods

--- libs/user-identity-service/src/main/java/uk/gov/di/ipv/core/library/useridentity/service/UserIdentityService.java
+++ libs/user-identity-service/src/main/java/uk/gov/di/ipv/core/library/useridentity/service/UserIdentityService.java
@@ -145,18 +145,6 @@ public class UserIdentityService {
         return false;
     }
 
-    public boolean areNamesCorrelated(List<VerifiableCredential> vcs)
-            throws HttpResponseExceptionWithErrorBody {
-        var successfulVcs = getSuccessfulVcs(vcs);
-        return checkNameAndFamilyNameCorrelationInCredentials(successfulVcs);
-    }
-
-    public boolean areDoBsCorrelated(List<VerifiableCredential> vcs)
-            throws HttpResponseExceptionWithErrorBody {
-        var successfulVcs = getSuccessfulVcs(vcs);
-        return checkBirthDateCorrelationInCredentials(successfulVcs);
-    }
-
     public boolean areNamesAndDobCorrelatedForReverification(List<VerifiableCredential> vcs)
             throws HttpResponseExceptionWithErrorBody {
         var successfulVcs = getSuccessfulVcs(vcs);
